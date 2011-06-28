a36d2ea42fe9524a474c678dce66cd964356d771
guava.mirrorbot@gmail.com <guava.mirrorbot@gmail.com@8138a162-5c33-11de-8abc-d1c337b90d21>
2011-06-28 15:25:42 +0000

    Moved UncheckedExecutionException to util/concurrent.
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=2416
    
    
    git-svn-id: https://guava-libraries.googlecode.com/svn/trunk@501 8138a162-5c33-11de-8abc-d1c337b90d21
    
    Former-commit-id: f1dd5f59b4c796a5165d3e4063a1ffd568272c0a

--- guava/src/com/google/common/util/concurrent/Futures.java
+++ guava/src/com/google/common/util/concurrent/Futures.java
@@ -176,20 +176,6 @@ end_import
 
 begin_import
 import|import
-name|com
-operator|.
-name|google
-operator|.
-name|common
-operator|.
-name|collect
-operator|.
-name|UncheckedExecutionException
-import|;
-end_import
-
-begin_import
-import|import
 name|java
 operator|.
 name|lang
---
+++ guava/src/com/google/common/util/concurrent/UncheckedExecutionException.java
@@ -0,0 +1,102 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Copyright (C) 2011 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|com.google.common.util.concurrent
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|common
+operator|.
+name|util
+operator|.
+name|concurrent
+package|;
+end_package
+
+begin_comment
+comment|/**  * Unchecked version of {@link java.util.concurrent.ExecutionException}. As with  * {@code ExecutionException}, the exception's {@linkplain #getCause() cause}  * comes from a failed task, possibly run in another thread.  *  *<p>{@code UncheckedExecutionException} is intended for users of {@code  * Future} and similar classes who know that their tasks will never throw a  * checked exception. They have the option of removing {@code throws  * ExecutionException} from their signatures, wrapping any exceptions that do  * occur in an {@code UncheckedExecutionException}.  *  * @author fry@google.com (Charles Fry)  * @since Guava release 10  */
+end_comment
+
+begin_class
+DECL|class|UncheckedExecutionException
+specifier|public
+class|class
+name|UncheckedExecutionException
+extends|extends
+name|RuntimeException
+block|{
+comment|/**    * Creates a new instance with {@code null} as its detail message.    */
+DECL|method|UncheckedExecutionException ()
+specifier|protected
+name|UncheckedExecutionException
+parameter_list|()
+block|{}
+comment|/**    * Creates a new instance with the given detail message.    */
+DECL|method|UncheckedExecutionException (String message)
+specifier|protected
+name|UncheckedExecutionException
+parameter_list|(
+name|String
+name|message
+parameter_list|)
+block|{
+name|super
+argument_list|(
+name|message
+argument_list|)
+expr_stmt|;
+block|}
+comment|/**    * Creates a new instance with the given detail message and cause.    */
+DECL|method|UncheckedExecutionException (String message, Throwable cause)
+specifier|public
+name|UncheckedExecutionException
+parameter_list|(
+name|String
+name|message
+parameter_list|,
+name|Throwable
+name|cause
+parameter_list|)
+block|{
+name|super
+argument_list|(
+name|message
+argument_list|,
+name|cause
+argument_list|)
+expr_stmt|;
+block|}
+comment|/**    * Creates a new instance with the given cause.    */
+DECL|method|UncheckedExecutionException (Throwable cause)
+specifier|public
+name|UncheckedExecutionException
+parameter_list|(
+name|Throwable
+name|cause
+parameter_list|)
+block|{
+name|super
+argument_list|(
+name|cause
+argument_list|)
+expr_stmt|;
+block|}
+DECL|field|serialVersionUID
+specifier|private
+specifier|static
+specifier|final
+name|long
+name|serialVersionUID
+init|=
+literal|0
+decl_stmt|;
+block|}
+end_class
+
+end_unit
+
