f946f51a1898476eff23892ec41fd5f4f0655751
guava.mirrorbot@gmail.com <guava.mirrorbot@gmail.com@8138a162-5c33-11de-8abc-d1c337b90d21>
2011-06-30 21:03:17 +0000

    Bug fix in Interners#newWeakInterner.
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=2475
    
    
    git-svn-id: https://guava-libraries.googlecode.com/svn/trunk@513 8138a162-5c33-11de-8abc-d1c337b90d21
    
    Former-commit-id: c8f276037d02c63b6860031feedc39642b4c6131

--- guava/src/com/google/common/collect/Interners.java
+++ guava/src/com/google/common/collect/Interners.java
@@ -300,7 +300,7 @@ name|entry
 init|=
 name|map
 operator|.
-name|getLiveEntry
+name|getEntry
 argument_list|(
 name|sample
 argument_list|)
@@ -312,12 +312,26 @@ operator|!=
 literal|null
 condition|)
 block|{
-return|return
+name|E
+name|canonical
+init|=
 name|entry
 operator|.
 name|getKey
 argument_list|()
+decl_stmt|;
+if|if
+condition|(
+name|canonical
+operator|!=
+literal|null
+condition|)
+block|{
+comment|// only matters if weak/soft keys are used
+return|return
+name|canonical
 return|;
+block|}
 block|}
 comment|// didn't see it, trying to put it instead...
 name|Dummy
