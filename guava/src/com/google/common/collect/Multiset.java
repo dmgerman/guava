4a229e52cdefdc1ba7e35fab2f4874bc84fc2e21
guava.mirrorbot@gmail.com <guava.mirrorbot@gmail.com@8138a162-5c33-11de-8abc-d1c337b90d21>
2011-07-07 16:19:15 +0000

    Implement Multisets.removeOccurrences and retainOccurrences
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=2511
    
    
    git-svn-id: https://guava-libraries.googlecode.com/svn/trunk@518 8138a162-5c33-11de-8abc-d1c337b90d21
    
    Former-commit-id: 457b295e7bc6f6ad15b3abe7149bf890d25ab1b6

--- guava/src/com/google/common/collect/Multiset.java
+++ guava/src/com/google/common/collect/Multiset.java
@@ -343,7 +343,7 @@ name|Object
 name|element
 parameter_list|)
 function_decl|;
-comment|/**    * {@inheritDoc}    *    *<p>This method refines {@link Collection#removeAll} to further specify that    * it<b>may not</b> throw an exception in response to any of {@code elements}    * being null or of the wrong type.    */
+comment|/**    * {@inheritDoc}    *    *<p><b>Note</b>: This method ignores how often any element might appear in     * {@code c}, and only cares whether or not an element appears at all.    * If you wish to remove one occurrence in this multiset for every occurrence    * in {@code c}, see {@link Multisets#removeOccurrences(Multiset, Multiset)}.    *     *<p>This method refines {@link Collection#removeAll} to further specify that    * it<b>may not</b> throw an exception in response to any of {@code elements}    * being null or of the wrong type.     */
 annotation|@
 name|Override
 DECL|method|removeAll (Collection<?> c)
@@ -357,7 +357,7 @@ argument_list|>
 name|c
 parameter_list|)
 function_decl|;
-comment|/**    * {@inheritDoc}    *    *<p>This method refines {@link Collection#retainAll} to further specify that    * it<b>may not</b> throw an exception in response to any of {@code elements}    * being null or of the wrong type.    */
+comment|/**    * {@inheritDoc}    *     *<p><b>Note</b>: This method ignores how often any element might appear in     * {@code c}, and only cares whether or not an element appears at all.    * If you wish to remove one occurrence in this multiset for every occurrence    * in {@code c}, see {@link Multisets#retainOccurrences(Multiset, Multiset)}.    *     *<p>This method refines {@link Collection#retainAll} to further specify that    * it<b>may not</b> throw an exception in response to any of {@code elements}    * being null or of the wrong type.    *     * @see Multisets#retainOccurrences(Multiset, Multiset)    */
 annotation|@
 name|Override
 DECL|method|retainAll (Collection<?> c)
