6423a94611e9abc86faa06a869fc6206e3687566
guava.mirrorbot@gmail.com <guava.mirrorbot@gmail.com@8138a162-5c33-11de-8abc-d1c337b90d21>
2011-08-04 14:06:40 +0000

    add common.collect.testing to Guava
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=2879
    
    
    git-svn-id: https://guava-libraries.googlecode.com/svn/trunk@610 8138a162-5c33-11de-8abc-d1c337b90d21
    
    Former-commit-id: 616b0430d73f5dfc602c6e4fb17c60288e0b480f

---
+++ guava-testlib/src/com/google/common/collect/testing/WrongType.java
@@ -0,0 +1,37 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Copyright (C) 2007 Google Inc.  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|com.google.common.collect.testing
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|common
+operator|.
+name|collect
+operator|.
+name|testing
+package|;
+end_package
+
+begin_comment
+comment|/**  * A type which will never be used as the element type of any collection in our  * tests, and so can be used to test how a Collection behaves when given input  * of the wrong type.  *  *<p>This class is GWT compatible.  */
+end_comment
+
+begin_enum
+DECL|enum|WrongType
+specifier|public
+enum|enum
+name|WrongType
+block|{
+DECL|enumConstant|VALUE
+name|VALUE
+block|}
+end_enum
+
+end_unit
+
