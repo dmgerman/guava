390f3cd9792f14f09a88d590a1896a92a09b127a
Charles Fry <fry@google.com>
2011-10-12 10:54:04 -0400

    Add c.g.c.net.HttpHeaders to Guava (since 11.0). ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=24584184
    Former-commit-id: 7ecaf592b89798c9d35c056cd957e3522354bdbb

---
+++ guava/src/com/google/common/net/HttpHeaders.java
@@ -0,0 +1,815 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Copyright (C) 2011 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|com.google.common.net
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|common
+operator|.
+name|net
+package|;
+end_package
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|common
+operator|.
+name|annotations
+operator|.
+name|Beta
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|common
+operator|.
+name|annotations
+operator|.
+name|GwtCompatible
+import|;
+end_import
+
+begin_comment
+comment|/**  * Contains constant definitions for the HTTP header field names. See:  *<ul>  *<li><a href="http://www.ietf.org/rfc/rfc2109.txt">RFC 2109</a>  *<li><a href="http://www.ietf.org/rfc/rfc2183.txt">RFC 2183</a>  *<li><a href="http://www.ietf.org/rfc/rfc2616.txt">RFC 2616</a>  *<li><a href="http://www.ietf.org/rfc/rfc2965.txt">RFC 2965</a>  *<li><a href="http://www.ietf.org/rfc/rfc5988.txt">RFC 5988</a>  *</ul>  *  * @author Kurt Alfred Kluever  * @since 11.0  */
+end_comment
+
+begin_class
+annotation|@
+name|Beta
+annotation|@
+name|GwtCompatible
+DECL|class|HttpHeaders
+specifier|public
+specifier|final
+class|class
+name|HttpHeaders
+block|{
+DECL|method|HttpHeaders ()
+specifier|private
+name|HttpHeaders
+parameter_list|()
+block|{}
+comment|// HTTP Request and Response header fields
+comment|/** The HTTP Cache-Control header field name. */
+DECL|field|CACHE_CONTROL
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|CACHE_CONTROL
+init|=
+literal|"Cache-Control"
+decl_stmt|;
+comment|/** The HTTP Content-Length header field name. */
+DECL|field|CONTENT_LENGTH
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|CONTENT_LENGTH
+init|=
+literal|"Content-Length"
+decl_stmt|;
+comment|/** The HTTP Content-Type header field name. */
+DECL|field|CONTENT_TYPE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|CONTENT_TYPE
+init|=
+literal|"Content-Type"
+decl_stmt|;
+comment|/** The HTTP Date header field name. */
+DECL|field|DATE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|DATE
+init|=
+literal|"Date"
+decl_stmt|;
+comment|/** The HTTP Pragma header field name. */
+DECL|field|PRAGMA
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|PRAGMA
+init|=
+literal|"Pragma"
+decl_stmt|;
+comment|/** The HTTP Via header field name. */
+DECL|field|VIA
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|VIA
+init|=
+literal|"Via"
+decl_stmt|;
+comment|/** The HTTP Warning header field name. */
+DECL|field|WARNING
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|WARNING
+init|=
+literal|"Warning"
+decl_stmt|;
+comment|// HTTP Request header fields
+comment|/** The HTTP Accept header field name. */
+DECL|field|ACCEPT
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ACCEPT
+init|=
+literal|"Accept"
+decl_stmt|;
+comment|/** The HTTP Accept-Charset header field name. */
+DECL|field|ACCEPT_CHARSET
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ACCEPT_CHARSET
+init|=
+literal|"Accept-Charset"
+decl_stmt|;
+comment|/** The HTTP Accept-Encoding header field name. */
+DECL|field|ACCEPT_ENCODING
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ACCEPT_ENCODING
+init|=
+literal|"Accept-Encoding"
+decl_stmt|;
+comment|/** The HTTP Accept-Language header field name. */
+DECL|field|ACCEPT_LANGUAGE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ACCEPT_LANGUAGE
+init|=
+literal|"Accept-Language"
+decl_stmt|;
+comment|/** The HTTP Access-Control-Request-Headers header field name. */
+DECL|field|ACCESS_CONTROL_REQUEST_HEADERS
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ACCESS_CONTROL_REQUEST_HEADERS
+init|=
+literal|"Access-Control-Request-Headers"
+decl_stmt|;
+comment|/** The HTTP Access-Control-Request-Method header field name. */
+DECL|field|ACCESS_CONTROL_REQUEST_METHOD
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ACCESS_CONTROL_REQUEST_METHOD
+init|=
+literal|"Access-Control-Request-Method"
+decl_stmt|;
+comment|/** The HTTP Authorization header field name. */
+DECL|field|AUTHORIZATION
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|AUTHORIZATION
+init|=
+literal|"Authorization"
+decl_stmt|;
+comment|/** The HTTP Connection header field name. */
+DECL|field|CONNECTION
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|CONNECTION
+init|=
+literal|"Connection"
+decl_stmt|;
+comment|/** The HTTP Cookie header field name. */
+DECL|field|COOKIE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|COOKIE
+init|=
+literal|"Cookie"
+decl_stmt|;
+comment|/** The HTTP Expect header field name. */
+DECL|field|EXPECT
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|EXPECT
+init|=
+literal|"Expect"
+decl_stmt|;
+comment|/** The HTTP From header field name. */
+DECL|field|FROM
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|FROM
+init|=
+literal|"From"
+decl_stmt|;
+comment|/** The HTTP Host header field name. */
+DECL|field|HOST
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|HOST
+init|=
+literal|"Host"
+decl_stmt|;
+comment|/** The HTTP If-Match header field name. */
+DECL|field|IF_MATCH
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|IF_MATCH
+init|=
+literal|"If-Match"
+decl_stmt|;
+comment|/** The HTTP If-Modified-Since header field name. */
+DECL|field|IF_MODIFIED_SINCE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|IF_MODIFIED_SINCE
+init|=
+literal|"If-Modified-Since"
+decl_stmt|;
+comment|/** The HTTP If-None-Match header field name. */
+DECL|field|IF_NONE_MATCH
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|IF_NONE_MATCH
+init|=
+literal|"If-None-Match"
+decl_stmt|;
+comment|/** The HTTP If-Range header field name. */
+DECL|field|IF_RANGE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|IF_RANGE
+init|=
+literal|"If-Range"
+decl_stmt|;
+comment|/** The HTTP If-Unmodified-Since header field name. */
+DECL|field|IF_UNMODIFIED_SINCE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|IF_UNMODIFIED_SINCE
+init|=
+literal|"If-Unmodified-Since"
+decl_stmt|;
+comment|/** The HTTP Last-Event-ID header field name. */
+DECL|field|LAST_EVENT_ID
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|LAST_EVENT_ID
+init|=
+literal|"Last-Event-ID"
+decl_stmt|;
+comment|/** The HTTP Max-Forwards header field name. */
+DECL|field|MAX_FORWARDS
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|MAX_FORWARDS
+init|=
+literal|"Max-Forwards"
+decl_stmt|;
+comment|/** The HTTP Origin header field name. */
+DECL|field|ORIGIN
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ORIGIN
+init|=
+literal|"Origin"
+decl_stmt|;
+comment|/** The HTTP Proxy-Authorization header field name. */
+DECL|field|PROXY_AUTHORIZATION
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|PROXY_AUTHORIZATION
+init|=
+literal|"Proxy-Authorization"
+decl_stmt|;
+comment|/** The HTTP Range header field name. */
+DECL|field|RANGE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|RANGE
+init|=
+literal|"Range"
+decl_stmt|;
+comment|/** The HTTP Referer header field name. */
+DECL|field|REFERER
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|REFERER
+init|=
+literal|"Referer"
+decl_stmt|;
+comment|/** The HTTP TE header field name. */
+DECL|field|TE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|TE
+init|=
+literal|"TE"
+decl_stmt|;
+comment|/** The HTTP Upgrade header field name. */
+DECL|field|UPGRADE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|UPGRADE
+init|=
+literal|"Upgrade"
+decl_stmt|;
+comment|/** The HTTP User-Agent header field name. */
+DECL|field|USER_AGENT
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|USER_AGENT
+init|=
+literal|"User-Agent"
+decl_stmt|;
+comment|// HTTP Response header fields
+comment|/** The HTTP Accept-Ranges header field name. */
+DECL|field|ACCEPT_RANGES
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ACCEPT_RANGES
+init|=
+literal|"Accept-Ranges"
+decl_stmt|;
+comment|/** The HTTP Access-Control-Allow-Headers header field name. */
+DECL|field|ACCESS_CONTROL_ALLOW_HEADERS
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ACCESS_CONTROL_ALLOW_HEADERS
+init|=
+literal|"Access-Control-Allow-Headers"
+decl_stmt|;
+comment|/** The HTTP Access-Control-Allow-Methods header field name. */
+DECL|field|ACCESS_CONTROL_ALLOW_METHODS
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ACCESS_CONTROL_ALLOW_METHODS
+init|=
+literal|"Access-Control-Allow-Methods"
+decl_stmt|;
+comment|/** The HTTP Access-Control-Allow-Origin header field name. */
+DECL|field|ACCESS_CONTROL_ALLOW_ORIGIN
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ACCESS_CONTROL_ALLOW_ORIGIN
+init|=
+literal|"Access-Control-Allow-Origin"
+decl_stmt|;
+comment|/** The HTTP Access-Control-Allow-Credentials header field name. */
+DECL|field|ACCESS_CONTROL_ALLOW_CREDENTIALS
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ACCESS_CONTROL_ALLOW_CREDENTIALS
+init|=
+literal|"Access-Control-Allow-Credentials"
+decl_stmt|;
+comment|/** The HTTP Access-Control-Expose-Headers header field name. */
+DECL|field|ACCESS_CONTROL_EXPOSE_HEADERS
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ACCESS_CONTROL_EXPOSE_HEADERS
+init|=
+literal|"Access-Control-Expose-Headers"
+decl_stmt|;
+comment|/** The HTTP Access-Control-Max-Age header field name. */
+DECL|field|ACCESS_CONTROL_MAX_AGE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ACCESS_CONTROL_MAX_AGE
+init|=
+literal|"Access-Control-Max-Age"
+decl_stmt|;
+comment|/** The HTTP Age header field name. */
+DECL|field|AGE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|AGE
+init|=
+literal|"Age"
+decl_stmt|;
+comment|/** The HTTP Allow header field name. */
+DECL|field|ALLOW
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ALLOW
+init|=
+literal|"Allow"
+decl_stmt|;
+comment|/** The HTTP Content-Encoding header field name. */
+DECL|field|CONTENT_ENCODING
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|CONTENT_ENCODING
+init|=
+literal|"Content-Encoding"
+decl_stmt|;
+comment|/** The HTTP Content-Language header field name. */
+DECL|field|CONTENT_LANGUAGE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|CONTENT_LANGUAGE
+init|=
+literal|"Content-Language"
+decl_stmt|;
+comment|/** The HTTP Content-Location header field name. */
+DECL|field|CONTENT_LOCATION
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|CONTENT_LOCATION
+init|=
+literal|"Content-Location"
+decl_stmt|;
+comment|/** The HTTP Content-Disposition header field name. */
+DECL|field|CONTENT_DISPOSITION
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|CONTENT_DISPOSITION
+init|=
+literal|"Content-Disposition"
+decl_stmt|;
+comment|/** The HTTP Content-MD5 header field name. */
+DECL|field|CONTENT_MD5
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|CONTENT_MD5
+init|=
+literal|"Content-MD5"
+decl_stmt|;
+comment|/** The HTTP Content-Range header field name. */
+DECL|field|CONTENT_RANGE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|CONTENT_RANGE
+init|=
+literal|"Content-Range"
+decl_stmt|;
+comment|/** The HTTP ETag header field name. */
+DECL|field|ETAG
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ETAG
+init|=
+literal|"ETag"
+decl_stmt|;
+comment|/** The HTTP Expires header field name. */
+DECL|field|EXPIRES
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|EXPIRES
+init|=
+literal|"Expires"
+decl_stmt|;
+comment|/** The HTTP Last-Modified header field name. */
+DECL|field|LAST_MODIFIED
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|LAST_MODIFIED
+init|=
+literal|"Last-Modified"
+decl_stmt|;
+comment|/** The HTTP Link header field name. */
+DECL|field|LINK
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|LINK
+init|=
+literal|"Link"
+decl_stmt|;
+comment|/** The HTTP Location header field name. */
+DECL|field|LOCATION
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|LOCATION
+init|=
+literal|"Location"
+decl_stmt|;
+comment|/** The HTTP P3P header field name. Limited browser support. */
+DECL|field|P3P
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|P3P
+init|=
+literal|"P3P"
+decl_stmt|;
+comment|/** The HTTP Proxy-Authenticate header field name. */
+DECL|field|PROXY_AUTHENTICATE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|PROXY_AUTHENTICATE
+init|=
+literal|"Proxy-Authenticate"
+decl_stmt|;
+comment|/** The HTTP Refresh header field name. Non-standard header supported by most browsers. */
+DECL|field|REFRESH
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|REFRESH
+init|=
+literal|"Refresh"
+decl_stmt|;
+comment|/** The HTTP Retry-After header field name. */
+DECL|field|RETRY_AFTER
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|RETRY_AFTER
+init|=
+literal|"Retry-After"
+decl_stmt|;
+comment|/** The HTTP Server header field name. */
+DECL|field|SERVER
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|SERVER
+init|=
+literal|"Server"
+decl_stmt|;
+comment|/** The HTTP Set-Cookie header field name. */
+DECL|field|SET_COOKIE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|SET_COOKIE
+init|=
+literal|"Set-Cookie"
+decl_stmt|;
+comment|/** The HTTP Set-Cookie2 header field name. */
+DECL|field|SET_COOKIE2
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|SET_COOKIE2
+init|=
+literal|"Set-Cookie2"
+decl_stmt|;
+comment|/** The HTTP Trailer header field name. */
+DECL|field|TRAILER
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|TRAILER
+init|=
+literal|"Trailer"
+decl_stmt|;
+comment|/** The HTTP Transfer-Encoding header field name. */
+DECL|field|TRANSFER_ENCODING
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|TRANSFER_ENCODING
+init|=
+literal|"Transfer-Encoding"
+decl_stmt|;
+comment|/** The HTTP Vary header field name. */
+DECL|field|VARY
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|VARY
+init|=
+literal|"Vary"
+decl_stmt|;
+comment|/** The HTTP WWW-Authenticate header field name. */
+DECL|field|WWW_AUTHENTICATE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|WWW_AUTHENTICATE
+init|=
+literal|"WWW-Authenticate"
+decl_stmt|;
+comment|// Common, non-standard HTTP header fields
+comment|/** The HTTP DNT header field name. */
+DECL|field|DNT
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|DNT
+init|=
+literal|"DNT"
+decl_stmt|;
+comment|/** The HTTP X-Content-Type-Options header field name. */
+DECL|field|X_CONTENT_TYPE_OPTIONS
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|X_CONTENT_TYPE_OPTIONS
+init|=
+literal|"X-Content-Type-Options"
+decl_stmt|;
+comment|/** The HTTP X-Do-Not-Track header field name. */
+DECL|field|X_DO_NOT_TRACK
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|X_DO_NOT_TRACK
+init|=
+literal|"X-Do-Not-Track"
+decl_stmt|;
+comment|/** The HTTP X-Forwarded-For header field name. */
+DECL|field|X_FORWARDED_FOR
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|X_FORWARDED_FOR
+init|=
+literal|"X-Forwarded-For"
+decl_stmt|;
+comment|/** The HTTP X-Forwarded-Proto header field name. */
+DECL|field|X_FORWARDED_PROTO
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|X_FORWARDED_PROTO
+init|=
+literal|"X-Forwarded-Proto"
+decl_stmt|;
+comment|/** The HTTP X-Frame-Options header field name. */
+DECL|field|X_FRAME_OPTIONS
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|X_FRAME_OPTIONS
+init|=
+literal|"X-Frame-Options"
+decl_stmt|;
+comment|/** The HTTP X-Powered-By header field name. */
+DECL|field|X_POWERED_BY
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|X_POWERED_BY
+init|=
+literal|"X-Powered-By"
+decl_stmt|;
+comment|/** The HTTP X-Requested-With header field name. */
+DECL|field|X_REQUESTED_WITH
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|X_REQUESTED_WITH
+init|=
+literal|"X-Requested-With"
+decl_stmt|;
+comment|/** The HTTP X-User-IP header field name. */
+DECL|field|X_USER_IP
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|X_USER_IP
+init|=
+literal|"X-User-IP"
+decl_stmt|;
+comment|/** The HTTP X-XSS-Protection header field name. */
+DECL|field|X_XSS_PROTECTION
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|X_XSS_PROTECTION
+init|=
+literal|"X-XSS-Protection"
+decl_stmt|;
+block|}
+end_class
+
+end_unit
+
