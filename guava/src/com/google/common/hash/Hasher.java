3cb6d980c9416a544ebb8202eb8f112215a1ff03
Charles Fry <fry@google.com>
2011-12-14 13:14:12 -0500

    Open source c.g.common.hash into Guava 11.0 ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=26225631
    Former-commit-id: f9318924b71d4ed2c59d3d835fe6a4ce3feefcbf

---
+++ guava/src/com/google/common/hash/Hasher.java
@@ -0,0 +1,225 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Copyright (C) 2011 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except  * in compliance with the License. You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software distributed under the License  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express  * or implied. See the License for the specific language governing permissions and limitations under  * the License.  */
+end_comment
+
+begin_package
+DECL|package|com.google.common.hash
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|common
+operator|.
+name|hash
+package|;
+end_package
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|common
+operator|.
+name|annotations
+operator|.
+name|Beta
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|nio
+operator|.
+name|charset
+operator|.
+name|Charset
+import|;
+end_import
+
+begin_comment
+comment|/**  * A {@link Sink} that can compute a hash code after reading the input. Each hasher should  * translate all multibyte values ({@link #putInt(int)}, {@link #putLong(long)}, etc) to bytes   * in little-endian order.  *  * @author Kevin Bourrillion  * @since 11.0  */
+end_comment
+
+begin_interface
+annotation|@
+name|Beta
+DECL|interface|Hasher
+specifier|public
+interface|interface
+name|Hasher
+extends|extends
+name|Sink
+block|{
+DECL|method|putByte (byte b)
+annotation|@
+name|Override
+name|Hasher
+name|putByte
+parameter_list|(
+name|byte
+name|b
+parameter_list|)
+function_decl|;
+DECL|method|putBytes (byte[] bytes)
+annotation|@
+name|Override
+name|Hasher
+name|putBytes
+parameter_list|(
+name|byte
+index|[]
+name|bytes
+parameter_list|)
+function_decl|;
+DECL|method|putBytes (byte[] bytes, int off, int len)
+annotation|@
+name|Override
+name|Hasher
+name|putBytes
+parameter_list|(
+name|byte
+index|[]
+name|bytes
+parameter_list|,
+name|int
+name|off
+parameter_list|,
+name|int
+name|len
+parameter_list|)
+function_decl|;
+DECL|method|putShort (short s)
+annotation|@
+name|Override
+name|Hasher
+name|putShort
+parameter_list|(
+name|short
+name|s
+parameter_list|)
+function_decl|;
+DECL|method|putInt (int i)
+annotation|@
+name|Override
+name|Hasher
+name|putInt
+parameter_list|(
+name|int
+name|i
+parameter_list|)
+function_decl|;
+DECL|method|putLong (long l)
+annotation|@
+name|Override
+name|Hasher
+name|putLong
+parameter_list|(
+name|long
+name|l
+parameter_list|)
+function_decl|;
+comment|/**    * Equivalent to {@code putInt(Float.floatToRawIntBits(f))}.    */
+DECL|method|putFloat (float f)
+annotation|@
+name|Override
+name|Hasher
+name|putFloat
+parameter_list|(
+name|float
+name|f
+parameter_list|)
+function_decl|;
+comment|/**    * Equivalent to {@code putLong(Double.doubleToRawLongBits(d))}.    */
+DECL|method|putDouble (double d)
+annotation|@
+name|Override
+name|Hasher
+name|putDouble
+parameter_list|(
+name|double
+name|d
+parameter_list|)
+function_decl|;
+comment|/**    * Equivalent to {@code putByte(b ? (byte) 1 : (byte) 0)}.    */
+DECL|method|putBoolean (boolean b)
+annotation|@
+name|Override
+name|Hasher
+name|putBoolean
+parameter_list|(
+name|boolean
+name|b
+parameter_list|)
+function_decl|;
+DECL|method|putChar (char c)
+annotation|@
+name|Override
+name|Hasher
+name|putChar
+parameter_list|(
+name|char
+name|c
+parameter_list|)
+function_decl|;
+comment|/**    * Equivalent to {@code putBytes(charSequence.toString().getBytes(Charsets.UTF_16LE)}.    */
+DECL|method|putString (CharSequence charSequence)
+annotation|@
+name|Override
+name|Hasher
+name|putString
+parameter_list|(
+name|CharSequence
+name|charSequence
+parameter_list|)
+function_decl|;
+comment|/**    * Equivalent to {@code putBytes(charSequence.toString().getBytes(charset)}.    */
+DECL|method|putString (CharSequence charSequence, Charset charset)
+annotation|@
+name|Override
+name|Hasher
+name|putString
+parameter_list|(
+name|CharSequence
+name|charSequence
+parameter_list|,
+name|Charset
+name|charset
+parameter_list|)
+function_decl|;
+comment|/**    * A simple convenience for {@code funnel.funnel(object, this)}.    */
+DECL|method|putObject (T instance, Funnel<? super T> funnel)
+parameter_list|<
+name|T
+parameter_list|>
+name|Hasher
+name|putObject
+parameter_list|(
+name|T
+name|instance
+parameter_list|,
+name|Funnel
+argument_list|<
+name|?
+super|super
+name|T
+argument_list|>
+name|funnel
+parameter_list|)
+function_decl|;
+comment|/**    * Computes a hash code based on the data that have been provided to this hasher. The result is    * unspecified if this method is called more than once on the same instance.     */
+DECL|method|hash ()
+name|HashCode
+name|hash
+parameter_list|()
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
---
+++ guava/src/com/google/common/hash/Sink.java
@@ -0,0 +1,180 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Copyright (C) 2011 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except  * in compliance with the License. You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software distributed under the License  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express  * or implied. See the License for the specific language governing permissions and limitations under  * the License.  */
+end_comment
+
+begin_package
+DECL|package|com.google.common.hash
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|common
+operator|.
+name|hash
+package|;
+end_package
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|common
+operator|.
+name|annotations
+operator|.
+name|Beta
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|nio
+operator|.
+name|charset
+operator|.
+name|Charset
+import|;
+end_import
+
+begin_comment
+comment|/**  * An object which can receive a stream of primitive values.  *   * @author Kevin Bourrillion  * @since 11.0  */
+end_comment
+
+begin_interface
+annotation|@
+name|Beta
+DECL|interface|Sink
+specifier|public
+interface|interface
+name|Sink
+block|{
+comment|/**    * Puts a byte into this sink.    *    * @param b a byte    * @return this instance    */
+DECL|method|putByte (byte b)
+name|Sink
+name|putByte
+parameter_list|(
+name|byte
+name|b
+parameter_list|)
+function_decl|;
+comment|/**    * Puts an array of bytes into this sink.    *    * @param bytes a byte array    * @return this instance    */
+DECL|method|putBytes (byte[] bytes)
+name|Sink
+name|putBytes
+parameter_list|(
+name|byte
+index|[]
+name|bytes
+parameter_list|)
+function_decl|;
+comment|/**    * Puts a chunk of an array of bytes into this sink. {@code bytes[off]} is the first byte written,    * {@code bytes[off + len - 1]} is the last.     *     * @param bytes a byte array    * @param off the start offset in the array    * @param len the number of bytes to write    * @return this instance     * @throws IndexOutOfBoundsException if {@code off< 0} or {@code off + len> bytes.length} or    *   {@code len< 0}    */
+DECL|method|putBytes (byte[] bytes, int off, int len)
+name|Sink
+name|putBytes
+parameter_list|(
+name|byte
+index|[]
+name|bytes
+parameter_list|,
+name|int
+name|off
+parameter_list|,
+name|int
+name|len
+parameter_list|)
+function_decl|;
+comment|/**    * Puts a short into this sink.    */
+DECL|method|putShort (short s)
+name|Sink
+name|putShort
+parameter_list|(
+name|short
+name|s
+parameter_list|)
+function_decl|;
+comment|/**    * Puts an int into this sink.    */
+DECL|method|putInt (int i)
+name|Sink
+name|putInt
+parameter_list|(
+name|int
+name|i
+parameter_list|)
+function_decl|;
+comment|/**    * Puts a long into this sink.    */
+DECL|method|putLong (long l)
+name|Sink
+name|putLong
+parameter_list|(
+name|long
+name|l
+parameter_list|)
+function_decl|;
+comment|/**    * Puts a float into this sink.    */
+DECL|method|putFloat (float f)
+name|Sink
+name|putFloat
+parameter_list|(
+name|float
+name|f
+parameter_list|)
+function_decl|;
+comment|/**    * Puts a double into this sink.    */
+DECL|method|putDouble (double d)
+name|Sink
+name|putDouble
+parameter_list|(
+name|double
+name|d
+parameter_list|)
+function_decl|;
+comment|/**    * Puts a boolean into this sink.    */
+DECL|method|putBoolean (boolean b)
+name|Sink
+name|putBoolean
+parameter_list|(
+name|boolean
+name|b
+parameter_list|)
+function_decl|;
+comment|/**    * Puts a character into this sink.    */
+DECL|method|putChar (char c)
+name|Sink
+name|putChar
+parameter_list|(
+name|char
+name|c
+parameter_list|)
+function_decl|;
+comment|/**    * Puts a string into this sink.    */
+DECL|method|putString (CharSequence charSequence)
+name|Sink
+name|putString
+parameter_list|(
+name|CharSequence
+name|charSequence
+parameter_list|)
+function_decl|;
+comment|/**    * Puts a string into this sink using the given charset.    */
+DECL|method|putString (CharSequence charSequence, Charset charset)
+name|Sink
+name|putString
+parameter_list|(
+name|CharSequence
+name|charSequence
+parameter_list|,
+name|Charset
+name|charset
+parameter_list|)
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
