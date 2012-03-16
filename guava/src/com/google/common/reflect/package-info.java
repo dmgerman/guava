2ea86014f40f5d7b213327ce8811f75c90e62609
Kurt Kluever <kak@google.com>
2012-03-16 18:08:51 -0400

    Add superclass safety check in getSupertype(), added @Beta and @since to TypeToken and Reflection. reflect needs no GWT compatibility, so there seems to be no BUILD dark magic to do? ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=28428778
    Former-commit-id: f4cdd87a6103131b1faaa6531ee3650dd790d47e

---
+++ guava/src/com/google/common/reflect/package-info.java
@@ -0,0 +1,33 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Copyright (C) 2012 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_comment
+comment|/**  * This package contains utilities to work with Java reflection.  * It is a part of the open-source  *<a href="http://guava-libraries.googlecode.com">Guava libraries</a>.  */
+end_comment
+
+begin_annotation
+annotation|@
+name|javax
+operator|.
+name|annotation
+operator|.
+name|ParametersAreNonnullByDefault
+end_annotation
+
+begin_package
+DECL|package|com.google.common.reflect
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|common
+operator|.
+name|reflect
+package|;
+end_package
+
+end_unit
+
