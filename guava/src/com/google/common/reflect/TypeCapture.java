d4b911e189a1978f21754dbc01d27fe864906f88
Chris Povirk <cpovirk@google.com>
2012-03-22 16:02:38 -0400

    Applied missing Apache license stuff to the files I forgot in previous round (oops) ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=28579696
    Former-commit-id: 1b23f4e098bcc4de59b5993b5577049bd2eebdd7

--- guava/src/com/google/common/reflect/TypeCapture.java
+++ guava/src/com/google/common/reflect/TypeCapture.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|// Copyright 2012 Google Inc. All Rights Reserved.
+comment|/*  * Copyright (C) 2012 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -57,7 +57,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Captures the actual type of {@code T}.  *  * @author benyu@google.com (Ben Yu)  */
+comment|/**  * Captures the actual type of {@code T}.  *  * @author Ben Yu  */
 end_comment
 
 begin_class
--- guava/src/com/google/common/reflect/TypeResolver.java
+++ guava/src/com/google/common/reflect/TypeResolver.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|// Copyright 2009 Google Inc. All Rights Reserved.
+comment|/*  * Copyright (C) 2009 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -225,7 +225,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * This class can be used by any generic super class to resolve one of its type  * parameter to the actual type argument used by the subclass, provided the type  * argument is carried on the class declaration.  *   * @author benyu@google.com (Jige Yu)  */
+comment|/**  * This class can be used by any generic super class to resolve one of its type  * parameter to the actual type argument used by the subclass, provided the type  * argument is carried on the class declaration.  *   * @author Ben Yu  */
 end_comment
 
 begin_class
