55a50028b21b24e574c226350b65b815f0edd53e
Chris Povirk <cpovirk@google.com>
2012-03-23 17:33:06 -0400

    Add Apache header to BloomFilterStrategies ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=28651552
    Former-commit-id: ea4d8f3a5756d920033728a4646fe80ac162119f

--- guava/src/com/google/common/hash/BloomFilterStrategies.java
+++ guava/src/com/google/common/hash/BloomFilterStrategies.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|// Copyright 2011 Google Inc. All Rights Reserved.
+comment|/*  * Copyright (C) 2011 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except  * in compliance with the License. You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software distributed under the License  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express  * or implied. See the License for the specific language governing permissions and limitations under  * the License.  */
 end_comment
 
 begin_package
@@ -67,7 +67,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Collections of strategies of generating the k * log(M) bits required for an element to  * be mapped to a BloomFilter of M bits and k hash functions. These  * strategies are part of the serialized form of the Bloom filters that use them, thus they must be  * preserved as is (no updates allowed, only introduction of new versions).  *  * Important: the order of the constants cannot change, and they cannot be deleted - we depend  * on their ordinal for BloomFilter serialization.  *  * @author andreou@google.com (Dimitris Andreou)  */
+comment|/**  * Collections of strategies of generating the k * log(M) bits required for an element to  * be mapped to a BloomFilter of M bits and k hash functions. These  * strategies are part of the serialized form of the Bloom filters that use them, thus they must be  * preserved as is (no updates allowed, only introduction of new versions).  *  * Important: the order of the constants cannot change, and they cannot be deleted - we depend  * on their ordinal for BloomFilter serialization.  *  * @author Dimitris Andreou  */
 end_comment
 
 begin_enum
