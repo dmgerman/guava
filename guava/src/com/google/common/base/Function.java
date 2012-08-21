8996870af8cd16d2eae4838021be1fc3355e3ad6
Kurt Kluever <kak@google.com>
2012-08-21 00:19:43 -0400

    Point from Function to Functions and from Predicate to Predicates. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=32953252
    Former-commit-id: 09c626ed9f63b409692c81c65febedd45e362a14

--- guava/src/com/google/common/base/Function.java
+++ guava/src/com/google/common/base/Function.java
@@ -41,7 +41,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Determines an output value based on an input value.  *   *<p>See the Guava User Guide article on<a href=  * "http://code.google.com/p/guava-libraries/wiki/FunctionalExplained">the use of {@code  * Function}</a>.  *   * @author Kevin Bourrillion  * @since 2.0 (imported from Google Collections Library)  */
+comment|/**  * Determines an output value based on an input value.  *  *<p>The {@link Functions} class provides common functions and related utilites.  *  *<p>See the Guava User Guide article on<a href=  * "http://code.google.com/p/guava-libraries/wiki/FunctionalExplained">the use of {@code  * Function}</a>.  *  * @author Kevin Bourrillion  * @since 2.0 (imported from Google Collections Library)  */
 end_comment
 
 begin_interface
