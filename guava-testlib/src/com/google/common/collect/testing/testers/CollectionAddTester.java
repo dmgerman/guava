39c598cc0fa38bfc9f716cbf8be2dfb19d57f358
Chris Povirk <cpovirk@google.com>
2012-08-31 16:03:30 -0400

    Add tests for ConcurrentSkipList{Map,Set} and for a TreeMap with no comparator. The latter requires suppressions for the same reason that our no-comparator TreeSet tests do: The first element is wrongly permitted to be inserted even if it's null. Also, fix up a few misnamed methods and a copy-pasted doc comment. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=33468471
    Former-commit-id: ae758e0347c5ca9cee84c5738411852c4960bd2f

--- guava-testlib/src/com/google/common/collect/testing/testers/CollectionAddTester.java
+++ guava-testlib/src/com/google/common/collect/testing/testers/CollectionAddTester.java
@@ -604,7 +604,7 @@ literal|"testAdd_nullSupported"
 argument_list|)
 return|;
 block|}
-comment|/**    * Returns the {@link Method} instance for {@link #testAdd_nullSupported()} so    * that tests of {@link    * java.util.Collections#checkedCollection(java.util.Collection, Class)} can    * suppress it with {@code FeatureSpecificTestSuiteBuilder.suppressing()}    * until<a    * href="http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=5045147">Sun    * bug 5045147</a> is fixed.    */
+comment|/**    * Returns the {@link Method} instance for {@link #testAdd_nullSupported()}    * so that tests of {@link java.util.TreeSet} can suppress it with {@code    * FeatureSpecificTestSuiteBuilder.suppressing()} until<a    * href="http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=5045147">Sun bug    * 5045147</a> is fixed.    */
 annotation|@
 name|GwtIncompatible
 argument_list|(
