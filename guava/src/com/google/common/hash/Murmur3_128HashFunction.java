709ed23fd5b3d5d8ca435b1cc3032a1b3fbbb1a4
Chris Povirk <cpovirk@google.com>
2012-10-29 14:22:11 -0400

    Add copyright information to ported Murmur hash functions. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=37032710
    Former-commit-id: 5efaa21fd4c318104bd5ef29f9979651f78fc3ba

--- guava/src/com/google/common/hash/Murmur3_128HashFunction.java
+++ guava/src/com/google/common/hash/Murmur3_128HashFunction.java
@@ -3,6 +3,14 @@ begin_comment
 comment|/*  * Copyright (C) 2011 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except  * in compliance with the License. You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software distributed under the License  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express  * or implied. See the License for the specific language governing permissions and limitations under  * the License.  */
 end_comment
 
+begin_comment
+comment|/*  * MurmurHash3 was written by Austin Appleby, and is placed in the public  * domain. The author hereby disclaims copyright to this source code.  */
+end_comment
+
+begin_comment
+comment|/*  * Source:  * http://code.google.com/p/smhasher/source/browse/trunk/MurmurHash3.cpp  * (Modified to adapt to Guava coding conventions and to use the HashFunction interface)  */
+end_comment
+
 begin_package
 DECL|package|com.google.common.hash
 package|package
