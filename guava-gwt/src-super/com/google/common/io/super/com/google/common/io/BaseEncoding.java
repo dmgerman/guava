64a7ddde009cc97d6eda4ac02aec4b4221fc3515
Chris Povirk <cpovirk@google.com>
2012-12-04 11:12:33 -0500

    Mention "hexadecimal" in BaseEncoding.base16(). ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=39617876
    Former-commit-id: 52bef6730688f74c0312ab396fcc6c6931070af6

--- guava-gwt/src-super/com/google/common/io/super/com/google/common/io/BaseEncoding.java
+++ guava-gwt/src-super/com/google/common/io/super/com/google/common/io/BaseEncoding.java
@@ -899,7 +899,7 @@ argument_list|,
 literal|null
 argument_list|)
 decl_stmt|;
-comment|/**    * The "base16" encoding specified by<a    * href="http://tools.ietf.org/html/rfc4648#section-8">RFC 4648 section 8</a>, Base 16 Encoding.    * (This is the same as the base 16 encoding from<a    * href="http://tools.ietf.org/html/rfc3548#section-6">RFC 3548</a>.)    *    *<p>No padding is necessary in base 16, so {@link #withPadChar(char)} and    * {@link #omitPadding()} have no effect.    *    *<p>No line feeds are added by default, as per<a    * href="http://tools.ietf.org/html/rfc4648#section-3.1"> RFC 4648 section 3.1</a>, Line Feeds in    * Encoded Data. Line feeds may be added using {@link #withSeparator(String, int)}.    */
+comment|/**    * The "base16" encoding specified by<a    * href="http://tools.ietf.org/html/rfc4648#section-8">RFC 4648 section 8</a>, Base 16 Encoding.    * (This is the same as the base 16 encoding from<a    * href="http://tools.ietf.org/html/rfc3548#section-6">RFC 3548</a>.) This is commonly known as    * "hexadecimal" format.    *    *<p>No padding is necessary in base 16, so {@link #withPadChar(char)} and    * {@link #omitPadding()} have no effect.    *    *<p>No line feeds are added by default, as per<a    * href="http://tools.ietf.org/html/rfc4648#section-3.1"> RFC 4648 section 3.1</a>, Line Feeds in    * Encoded Data. Line feeds may be added using {@link #withSeparator(String, int)}.    */
 DECL|method|base16 ()
 specifier|public
 specifier|static
