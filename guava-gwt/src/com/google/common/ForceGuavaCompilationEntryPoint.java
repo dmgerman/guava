1c905c8644ad24d1be487d1b50c36f22351b31e2
Chris Povirk <cpovirk@google.com>
2012-12-05 16:14:56 -0500

    Add GWT compilation test. Separate testlib GWT files into guava-testlib/blackhole-gwt, where they can be ignored for the time being. (testlib already doesn't work under GWT.) ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=39671586
    Former-commit-id: 133ffd3dd0d4967cb7f2d6f745b6661585c67b6b

---
+++ guava-gwt/src/com/google/common/ForceGuavaCompilationEntryPoint.java
@@ -0,0 +1,57 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Copyright (C) 2010 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|com.google.common
+package|package
+name|com
+operator|.
+name|google
+operator|.
+name|common
+package|;
+end_package
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|gwt
+operator|.
+name|core
+operator|.
+name|client
+operator|.
+name|EntryPoint
+import|;
+end_import
+
+begin_comment
+comment|/**  * A dummy entry point to convince Maven to compile our classes.  *  * @author Chris Povirk  */
+end_comment
+
+begin_class
+DECL|class|ForceGuavaCompilationEntryPoint
+specifier|public
+class|class
+name|ForceGuavaCompilationEntryPoint
+implements|implements
+name|EntryPoint
+block|{
+DECL|method|onModuleLoad ()
+annotation|@
+name|Override
+specifier|public
+name|void
+name|onModuleLoad
+parameter_list|()
+block|{   }
+block|}
+end_class
+
+end_unit
+
