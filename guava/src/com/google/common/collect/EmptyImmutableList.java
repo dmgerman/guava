675d639cf18112d57d02f71c40d80a90dfdb08de
Chris Povirk <cpovirk@google.com>
2013-01-11 12:47:01 -0500

    Rollback of controversial ee63a7a0994c [formerly c423bec3cc53] ("Trim gratuitous code from ImmutableList implementations"). ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=41031096
    Former-commit-id: 01c33a7487d8c3f90608ae5cd05debf62794c25b

--- guava/src/com/google/common/collect/EmptyImmutableList.java
+++ guava/src/com/google/common/collect/EmptyImmutableList.java
@@ -90,6 +90,16 @@ end_import
 
 begin_import
 import|import
+name|java
+operator|.
+name|util
+operator|.
+name|List
+import|;
+end_import
+
+begin_import
+import|import
 name|javax
 operator|.
 name|annotation
@@ -149,6 +159,18 @@ parameter_list|()
 block|{
 return|return
 literal|0
+return|;
+block|}
+DECL|method|isEmpty ()
+annotation|@
+name|Override
+specifier|public
+name|boolean
+name|isEmpty
+parameter_list|()
+block|{
+return|return
+literal|true
 return|;
 block|}
 DECL|method|isPartialView ()
@@ -418,6 +440,51 @@ return|return
 name|Iterators
 operator|.
 name|EMPTY_LIST_ITERATOR
+return|;
+block|}
+DECL|method|equals (@ullable Object object)
+annotation|@
+name|Override
+specifier|public
+name|boolean
+name|equals
+parameter_list|(
+annotation|@
+name|Nullable
+name|Object
+name|object
+parameter_list|)
+block|{
+if|if
+condition|(
+name|object
+operator|instanceof
+name|List
+condition|)
+block|{
+name|List
+argument_list|<
+name|?
+argument_list|>
+name|that
+init|=
+operator|(
+name|List
+argument_list|<
+name|?
+argument_list|>
+operator|)
+name|object
+decl_stmt|;
+return|return
+name|that
+operator|.
+name|isEmpty
+argument_list|()
+return|;
+block|}
+return|return
+literal|false
 return|;
 block|}
 DECL|method|hashCode ()
