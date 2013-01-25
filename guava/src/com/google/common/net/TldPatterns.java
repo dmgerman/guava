1bb4ca24731d71e4b61bbc42b866ed8841396b39
Christian Gruber <google.com>
2013-01-25 10:54:27 -0800

    Added cw to TldPatterns.java ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=41558410
    Former-commit-id: d08271f2cb67c1c659247327f0e5ca3bfe04b48e

--- guava/src/com/google/common/net/TldPatterns.java
+++ guava/src/com/google/common/net/TldPatterns.java
@@ -12418,6 +12418,8 @@ argument_list|,
 literal|"\u049b\u0430\u0437"
 argument_list|,
 literal|"xn--80ao21a"
+argument_list|,
+literal|"cw"
 argument_list|)
 decl_stmt|;
 comment|/**    * If a hostname is not in the EXCLUDE set, and if removing its    * leftmost component results in a name which is contained in this    * set, it is a TLD.    */
