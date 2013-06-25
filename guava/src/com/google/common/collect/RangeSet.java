a26510f4f02c919085cc1e9f3e47d3da8d3157d0
Chris Povirk <cpovirk@google.com>
2013-06-25 17:32:32 -0400

    Remove com.google.common.collect.Ranges (the static methods have been moved to com.google.common.collect.Range). ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=48509690
    Former-commit-id: cd9b3291a91b3b802930ebad062ed35b6d3d6347

--- guava/src/com/google/common/collect/RangeSet.java
+++ guava/src/com/google/common/collect/RangeSet.java
@@ -252,7 +252,7 @@ name|int
 name|hashCode
 parameter_list|()
 function_decl|;
-comment|/**    * Returns a readable string representation of this range set. For example, if this    * {@code RangeSet} consisted of {@code Ranges.closed(1, 3)} and {@code Ranges.greaterThan(4)},    * this might return {@code " [1â¥3](4â¥+â)}"}.    */
+comment|/**    * Returns a readable string representation of this range set. For example, if this    * {@code RangeSet} consisted of {@code Range.closed(1, 3)} and {@code Range.greaterThan(4)},    * this might return {@code " [1â¥3](4â¥+â)}"}.    */
 annotation|@
 name|Override
 DECL|method|toString ()
