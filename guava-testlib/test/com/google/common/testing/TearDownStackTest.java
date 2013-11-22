d6545c71844015816027d446f1a1c36719ae75d5
Chris Povirk <cpovirk@google.com>
2013-11-22 11:52:40 -0500

    Removed unnecessary static imports as they are pulled in through junit.framework.TestCase. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=57073259
    Former-commit-id: 73730b90eec6314b6c2dbf764bd2ccdf5d9c2a24

--- guava-testlib/test/com/google/common/testing/TearDownStackTest.java
+++ guava-testlib/test/com/google/common/testing/TearDownStackTest.java
@@ -17,30 +17,6 @@ package|;
 end_package
 
 begin_import
-import|import static
-name|org
-operator|.
-name|junit
-operator|.
-name|Assert
-operator|.
-name|assertEquals
-import|;
-end_import
-
-begin_import
-import|import static
-name|org
-operator|.
-name|junit
-operator|.
-name|Assert
-operator|.
-name|fail
-import|;
-end_import
-
-begin_import
 import|import
 name|com
 operator|.
