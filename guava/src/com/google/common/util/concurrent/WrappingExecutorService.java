745e58cded9de2102444da7b9fabad8c62ef6ee5
Colin Decker <cgdecker@google.com>
2014-01-03 11:38:48 -0500

    Clarify the relationship between the two wrapTask methods. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=59003211
    Former-commit-id: c81e4fa9760107230b1670bf74b334533a0302e8

--- guava/src/com/google/common/util/concurrent/WrappingExecutorService.java
+++ guava/src/com/google/common/util/concurrent/WrappingExecutorService.java
@@ -202,7 +202,7 @@ name|delegate
 argument_list|)
 expr_stmt|;
 block|}
-comment|/**    * Wraps a task before it is submitted to the underlying executor. Though    * specified in terms of {@link Callable}, this method is also applied to    * {@link Runnable} tasks (unless {@link #wrapTest(Runnable}} is overridden    * with different behavior).    */
+comment|/**    * Wraps a {@code Callable} for submission to the underlying executor. This    * method is also applied to any {@code Runnable} passed to the default    * implementation of {@link #wrapTest(Runnable)}.    */
 DECL|method|wrapTask (Callable<T> callable)
 specifier|protected
 specifier|abstract
@@ -222,7 +222,7 @@ argument_list|>
 name|callable
 parameter_list|)
 function_decl|;
-comment|/**    * Wraps a task before it is submitted to the underlying Executor.  This    * delegates to {@link #wrapTask(Callable)} by default, but should be    * overridden if there is a better implementation for {@link Runnable    * runnables}.    */
+comment|/**    * Wraps a {@code Runnable} for submission to the underlying executor. The    * default implementation delegates to {@link #wrapTask(Callable)}.    */
 DECL|method|wrapTask (Runnable command)
 specifier|protected
 name|Runnable
