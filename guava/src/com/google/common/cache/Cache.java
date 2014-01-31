da9c70cf97e11e203e2f7e66d9aea69c085d7cfe
Chris Povirk <cpovirk@google.com>
2014-01-31 13:28:42 -0500

    Promise at least weakly consistent iteration for asMap(). ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=60762175
    Former-commit-id: 7a8148a7bbd7aaa5e9e157118c824ab502f9b052

--- guava/src/com/google/common/cache/Cache.java
+++ guava/src/com/google/common/cache/Cache.java
@@ -281,7 +281,7 @@ name|CacheStats
 name|stats
 parameter_list|()
 function_decl|;
-comment|/**    * Returns a view of the entries stored in this cache as a thread-safe map. Modifications made to    * the map directly affect the cache.    */
+comment|/**    * Returns a view of the entries stored in this cache as a thread-safe map. Modifications made to    * the map directly affect the cache.    *    *<p>Iterators from the returned map are at least<i>weakly consistent</i>: they are safe for    * concurrent use, but if the cache is modified (including by eviction) after the iterator is    * created, it is undefined which of the changes (if any) will be reflected in that iterator.    */
 DECL|method|asMap ()
 name|ConcurrentMap
 argument_list|<
