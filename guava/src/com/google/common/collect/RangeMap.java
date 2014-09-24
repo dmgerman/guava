8c500e9c4cea3a2892c310709517d01d35c14615
Chris Povirk <cpovirk@google.com>
2014-09-24 11:52:01 -0700

    Establish RangeMap iteration order, as per Guava issue 1842. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=76272760
    Former-commit-id: 1c73b2d932d3951ebee432c11c0f5aac9ea1b696

--- guava/src/com/google/common/collect/RangeMap.java
+++ guava/src/com/google/common/collect/RangeMap.java
@@ -157,7 +157,7 @@ argument_list|>
 name|range
 parameter_list|)
 function_decl|;
-comment|/**    * Returns a view of this range map as an unmodifiable {@code Map<Range<K>, V>}.    * Modifications to this range map are guaranteed to read through to the returned {@code Map}.    *    *<p>It is guaranteed that no empty ranges will be in the returned {@code Map}.    */
+comment|/**    * Returns a view of this range map as an unmodifiable {@code Map<Range<K>, V>}.    * Modifications to this range map are guaranteed to read through to the returned {@code Map}.    *    *<p>The returned {@code Map} iterates over entries in ascending order of the bounds of the    * {@code Range} entries.    *    *<p>It is guaranteed that no empty ranges will be in the returned {@code Map}.    */
 DECL|method|asMapOfRanges ()
 name|Map
 argument_list|<
