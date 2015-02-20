562e3417c402285383ce2ff4f521c6d7e67da106
kak <kak@google.com>
2015-02-20 09:23:52 -0800

    Add @CheckReturnValue to Predicates. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=86794808
    Former-commit-id: d71cf1a7872259aa9701a0cc6bbadb47cacb29d1

--- guava-gwt/src-super/com/google/common/base/super/com/google/common/base/Predicates.java
+++ guava-gwt/src-super/com/google/common/base/super/com/google/common/base/Predicates.java
@@ -102,6 +102,16 @@ name|javax
 operator|.
 name|annotation
 operator|.
+name|CheckReturnValue
+import|;
+end_import
+
+begin_import
+import|import
+name|javax
+operator|.
+name|annotation
+operator|.
 name|Nullable
 import|;
 end_import
@@ -111,6 +121,8 @@ comment|/**  * Static utility methods pertaining to {@code Predicate} instances.
 end_comment
 
 begin_class
+annotation|@
+name|CheckReturnValue
 annotation|@
 name|GwtCompatible
 argument_list|(
--- guava/src/com/google/common/base/Predicates.java
+++ guava/src/com/google/common/base/Predicates.java
@@ -142,6 +142,16 @@ name|javax
 operator|.
 name|annotation
 operator|.
+name|CheckReturnValue
+import|;
+end_import
+
+begin_import
+import|import
+name|javax
+operator|.
+name|annotation
+operator|.
 name|Nullable
 import|;
 end_import
@@ -151,6 +161,8 @@ comment|/**  * Static utility methods pertaining to {@code Predicate} instances.
 end_comment
 
 begin_class
+annotation|@
+name|CheckReturnValue
 annotation|@
 name|GwtCompatible
 argument_list|(
