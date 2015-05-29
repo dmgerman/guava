3c62a498a79018c831dba7db12a1a991a8a0323f
lowasser <lowasser@google.com>
2015-05-29 13:29:18 -0700

    Add testers for ConcurrentMap.replace(K, V, V). ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=94784595
    Former-commit-id: 924be3363a15007dd3888ad85e878591412e4a68

--- guava-testlib/src/com/google/common/collect/testing/ConcurrentMapTestSuiteBuilder.java
+++ guava-testlib/src/com/google/common/collect/testing/ConcurrentMapTestSuiteBuilder.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (C) 2015 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Copyright (C) 2015 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except  * in compliance with the License. You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software distributed under the License  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express  * or implied. See the License for the specific language governing permissions and limitations under  * the License.  */
 end_comment
 
 begin_package
@@ -68,6 +68,24 @@ name|testing
 operator|.
 name|testers
 operator|.
+name|ConcurrentMapReplaceEntryTester
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|common
+operator|.
+name|collect
+operator|.
+name|testing
+operator|.
+name|testers
+operator|.
 name|ConcurrentMapReplaceTester
 import|;
 end_import
@@ -83,7 +101,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Creates, based on your criteria, a JUnit test suite that exhaustively tests  * a ConcurrentMap implementation.  *   * @author Louis Wasserman  */
+comment|/**  * Creates, based on your criteria, a JUnit test suite that exhaustively tests a ConcurrentMap  * implementation.  *  * @author Louis Wasserman  */
 end_comment
 
 begin_class
@@ -217,6 +235,15 @@ operator|.
 name|add
 argument_list|(
 name|ConcurrentMapReplaceTester
+operator|.
+name|class
+argument_list|)
+expr_stmt|;
+name|testers
+operator|.
+name|add
+argument_list|(
+name|ConcurrentMapReplaceEntryTester
 operator|.
 name|class
 argument_list|)
