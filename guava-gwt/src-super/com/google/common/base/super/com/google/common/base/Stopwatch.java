ff54a7c1685540f5609ae3bcbd8965a3566b0151
kak <kak@google.com>
2015-06-02 12:20:57 -0700

    De-@Beta Stopwatch.
    See https://github.com/google/guava/issues/1312 (we were holding off on the
    de-@Beta'ing because we wanted to remove the public constructors).
    
    The only other API change we might want to make is to add toString(TimeUnit)
    (https://github.com/google/guava/issues/2007) but that shouldn't stop us from
    de-@Beta'ing it now, right?
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=95031929
    
    
    Former-commit-id: 3bea24c6ad264e9a450debee9808993bdaa25ea5

--- guava-gwt/src-super/com/google/common/base/super/com/google/common/base/Stopwatch.java
+++ guava-gwt/src-super/com/google/common/base/super/com/google/common/base/Stopwatch.java
@@ -156,20 +156,6 @@ name|common
 operator|.
 name|annotations
 operator|.
-name|Beta
-import|;
-end_import
-
-begin_import
-import|import
-name|com
-operator|.
-name|google
-operator|.
-name|common
-operator|.
-name|annotations
-operator|.
 name|GwtCompatible
 import|;
 end_import
@@ -201,8 +187,6 @@ comment|/**  * An object that measures elapsed time in nanoseconds. It is useful
 end_comment
 
 begin_class
-annotation|@
-name|Beta
 annotation|@
 name|GwtCompatible
 argument_list|(
