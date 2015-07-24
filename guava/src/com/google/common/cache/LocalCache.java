85bfd29098950bce0ac63c420d5498aab865896a
kstanger <kstanger@google.com>
2015-07-24 10:12:43 -0700

    Fix cycles com.google.common.cache. Adds @J2ObjCIncompatible to com.google.common.util.Debugger. Stop whitelisting other packages that pass the cycle test. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=99038555
    Former-commit-id: f0c1ccba7e35bf46585f3fa4d1aaef00dc11c4ee

--- guava/src/com/google/common/cache/LocalCache.java
+++ guava/src/com/google/common/cache/LocalCache.java
@@ -506,6 +506,34 @@ end_import
 
 begin_import
 import|import
+name|com
+operator|.
+name|google
+operator|.
+name|j2objc
+operator|.
+name|annotations
+operator|.
+name|Weak
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|google
+operator|.
+name|j2objc
+operator|.
+name|annotations
+operator|.
+name|WeakOuter
+import|;
+end_import
+
+begin_import
+import|import
 name|java
 operator|.
 name|io
@@ -8425,6 +8453,8 @@ block|{
 comment|/*      * TODO(fry): Consider copying variables (like evictsBySize) from outer class into this class.      * It will require more memory but will reduce indirection.      */
 comment|/*      * Segments maintain a table of entry lists that are ALWAYS kept in a consistent state, so can      * be read without locking. Next fields of nodes are immutable (final). All list additions are      * performed at the front of each bin. This makes it easy to check changes, and also fast to      * traverse. When nodes would otherwise be changed, new nodes are created to replace them. This      * works well for hash tables since the bin lists tend to be short. (The average length is less      * than two.)      *      * Read operations can thus proceed without locking, but rely on selected uses of volatiles to      * ensure that completed write operations performed by other threads are noticed. For most      * purposes, the "count" field, tracking the number of elements, serves as that volatile      * variable ensuring visibility. This is convenient because this field needs to be read in many      * read operations anyway:      *      * - All (unsynchronized) read operations must first read the "count" field, and should not      * look at table entries if it is 0.      *      * - All (synchronized) write operations should write to the "count" field after structurally      * changing any bin. The operations must not take any action that could even momentarily      * cause a concurrent read operation to see inconsistent data. This is made easier by the      * nature of the read operations in Map. For example, no operation can reveal that the table      * has grown but the threshold has not yet been updated, so there are no atomicity requirements      * for this with respect to reads.      *      * As a guide, all critical volatile reads and writes to the count field are marked in code      * comments.      */
 DECL|field|map
+annotation|@
+name|Weak
 specifier|final
 name|LocalCache
 argument_list|<
@@ -20663,6 +20693,8 @@ name|T
 argument_list|>
 block|{
 DECL|field|map
+annotation|@
+name|Weak
 specifier|final
 name|ConcurrentMap
 argument_list|<
@@ -20736,6 +20768,8 @@ argument_list|()
 expr_stmt|;
 block|}
 block|}
+annotation|@
+name|WeakOuter
 DECL|class|KeySet
 specifier|final
 class|class
@@ -20824,6 +20858,8 @@ literal|null
 return|;
 block|}
 block|}
+annotation|@
+name|WeakOuter
 DECL|class|Values
 specifier|final
 class|class
@@ -20946,6 +20982,8 @@ argument_list|)
 return|;
 block|}
 block|}
+annotation|@
+name|WeakOuter
 DECL|class|EntrySet
 specifier|final
 class|class
