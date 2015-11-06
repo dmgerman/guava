9d79235134ab026726ccdf78f5371d193f5a5d3a
cpovirk <cpovirk@google.com>
2015-11-06 14:54:07 -0800

    Revise CL 103553690's scaretext, and propagate it to Cache.
    Also, reformat to 100 characters where not already done.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=107269241
    
    
    Former-commit-id: d3d3c98dcf1cd356c5afe1e5345b1398d5d1835c

--- guava/src/com/google/common/cache/Cache.java
+++ guava/src/com/google/common/cache/Cache.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (C) 2011 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Copyright (C) 2011 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except  * in compliance with the License. You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software distributed under the License  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express  * or implied. See the License for the specific language governing permissions and limitations under  * the License.  */
 end_comment
 
 begin_package
@@ -160,8 +160,8 @@ name|Object
 name|key
 parameter_list|)
 function_decl|;
-comment|/**    * Returns the value associated with {@code key} in this cache, obtaining that value from    * {@code valueLoader} if necessary. No observable state associated with this cache is modified    * until loading completes. This method provides a simple substitute for the conventional    * "if cached, return; otherwise create, cache and return" pattern.    *    *<p><b>Warning:</b> as with {@link CacheLoader#load}, {@code valueLoader}<b>must not</b> return    * {@code null}; it may either return a non-null value or throw an exception.    *    * @throws ExecutionException if a checked exception was thrown while loading the value    * @throws UncheckedExecutionException if an unchecked exception was thrown while loading the    *     value    * @throws ExecutionError if an error was thrown while loading the value    *    * @since 11.0    */
-DECL|method|get (K key, Callable<? extends V> valueLoader)
+comment|/**    * Returns the value associated with {@code key} in this cache, obtaining that value from {@code    * loader} if necessary. The method improves upon the conventional "if cached, return; otherwise    * create, cache and return" pattern. For further improvements, use {@link LoadingCache} and its    * {@link LoadingCache#get(Object) get(K)} method instead of this one.    *    *<p>Among the improvements that this method and {@code LoadingCache.get(K)} both provide are:    *    *<ul>    *<li>{@linkplain LoadingCache#get(Object) awaiting the result of a pending load} rather than    *     starting a redundant one    *<li>eliminating the error-prone caching boilerplate    *<li>tracking load {@linkplain #stats statistics}    *</ul>    *    *<p>Among the further improvements that {@code LoadingCache} can provide but this method cannot:    *    *<ul>    *<li>consolidation of the loader logic to {@linkplain CacheBuilder#build(CacheLoader) a single    *     authoritative location}    *<li>{@linkplain LoadingCache#refresh refreshing of entries}, including {@linkplain    *     CacheBuilder#refreshAfterWrite automated refreshing}    *<li>{@linkplain LoadingCache#getAll bulk loading requests}, including {@linkplain    *     CacheLoader#loadAll bulk loading implementations}    *</ul>    *    *<p><b>Warning:</b> For any given key, every {@code loader} used with it should compute the same    * value. Otherwise, a call that passes one {@code loader} may return the result of another call    * with a differently behaving {@code loader}. For example, a call that requests a short timeout    * for an RPC may wait for a similar call that requests a long timeout, or a call by an    * unprivileged user may return a resource accessible only to a privileged user making a similar    * call. To prevent this problem, create a key object that includes all values that affect the    * result of the query. Or use {@code LoadingCache.get(K)}, which lacks the ability to refer to    * state other than that in the key.    *    *<p><b>Warning:</b> as with {@link CacheLoader#load}, {@code loader}<b>must not</b> return    * {@code null}; it may either return a non-null value or throw an exception.    *    *<p>No observable state associated with this cache is modified until loading completes.    *    * @throws ExecutionException if a checked exception was thrown while loading the value    * @throws UncheckedExecutionException if an unchecked exception was thrown while loading the    *     value    * @throws ExecutionError if an error was thrown while loading the value    *    * @since 11.0    */
+DECL|method|get (K key, Callable<? extends V> loader)
 name|V
 name|get
 parameter_list|(
@@ -174,7 +174,7 @@ name|?
 extends|extends
 name|V
 argument_list|>
-name|valueLoader
+name|loader
 parameter_list|)
 throws|throws
 name|ExecutionException
@@ -209,7 +209,7 @@ name|value
 parameter_list|)
 function_decl|;
 comment|/**    * Copies all of the mappings from the specified map to the cache. The effect of this call is    * equivalent to that of calling {@code put(k, v)} on this map once for each mapping from key    * {@code k} to value {@code v} in the specified map. The behavior of this operation is undefined    * if the specified map is modified while the operation is in progress.    *    * @since 12.0    */
-DECL|method|putAll (Map<? extends K,? extends V> m)
+DECL|method|putAll (Map<? extends K, ? extends V> m)
 name|void
 name|putAll
 parameter_list|(
