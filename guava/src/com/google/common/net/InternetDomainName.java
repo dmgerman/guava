d4b302a902220d64dc8e5f3864ba47fe13cfe7ea
cpovirk <cpovirk@google.com>
2016-02-18 12:51:19 -0800

    Fix bad line breaks. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=114992502
    Former-commit-id: a6c137ef10842ee31a0749a121fbd44af4350559

--- guava/src/com/google/common/net/InternetDomainName.java
+++ guava/src/com/google/common/net/InternetDomainName.java
@@ -1046,7 +1046,7 @@ name|name
 argument_list|)
 return|;
 block|}
-comment|/**    * Indicates whether the argument is a syntactically valid domain name using lenient validation.    * Specifically, validation against<a href="http://www.ietf.org/rfc/rfc3490.txt">RFC 3490</a> (    * "Internationalizing Domain Names in Applications") is skipped.    *    *<p>The following two code snippets are equivalent:    *    *<pre>   {@code    *   domainName = InternetDomainName.isValid(name)    *       ? InternetDomainName.from(name)    *       : DEFAULT_DOMAIN;}</pre>    *    *<pre>   {@code    *   try {    *     domainName = InternetDomainName.from(name);    *   } catch (IllegalArgumentException e) {    *     domainName = DEFAULT_DOMAIN;    *   }}</pre>    *    * @since 8.0 (previously named {@code isValidLenient})    */
+comment|/**    * Indicates whether the argument is a syntactically valid domain name using lenient validation.    * Specifically, validation against<a href="http://www.ietf.org/rfc/rfc3490.txt">RFC 3490</a>    * ("Internationalizing Domain Names in Applications") is skipped.    *    *<p>The following two code snippets are equivalent:    *    *<pre>   {@code    *   domainName = InternetDomainName.isValid(name)    *       ? InternetDomainName.from(name)    *       : DEFAULT_DOMAIN;}</pre>    *    *<pre>   {@code    *   try {    *     domainName = InternetDomainName.from(name);    *   } catch (IllegalArgumentException e) {    *     domainName = DEFAULT_DOMAIN;    *   }}</pre>    *    * @since 8.0 (previously named {@code isValidLenient})    */
 DECL|method|isValid (String name)
 specifier|public
 specifier|static
