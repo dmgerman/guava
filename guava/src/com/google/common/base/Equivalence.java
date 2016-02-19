440f194d3b3221ec64f11673d06aba6f72240c29
kevinb <kevinb@google.com>
2016-02-19 11:15:20 -0800

    Attempt to finish adding Java 8 usage notes to our functional types. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=115081604
    Former-commit-id: aef3838801686022ad766b0ccfd21d5d45d0cb5c

--- guava/src/com/google/common/base/Equivalence.java
+++ guava/src/com/google/common/base/Equivalence.java
@@ -81,7 +81,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * A strategy for determining whether two instances are considered equivalent. Examples of  * equivalences are the {@linkplain #identity() identity equivalence} and {@linkplain #equals equals  * equivalence}.  *  * @author Bob Lee  * @author Ben Yu  * @author Gregory Kick  * @since 10.0 (<a href="https://github.com/google/guava/wiki/Compatibility">mostly  *     source-compatible</a> since 4.0)  */
+comment|/**  * A strategy for determining whether two instances are considered equivalent. Examples of  * equivalences are the {@linkplain #identity() identity equivalence} and {@linkplain #equals equals  * equivalence}.  *  *<h3>For Java 8+ users</h3>  *  *<p>A future version of this class will implement {@code BiPredicate<T, T>}. In the meantime, to  * use an equivalence (say, named {@code equivalence}) as a bi-predicate, use the method reference  * {@code equivalence::equivalent}.  *   * @author Bob Lee  * @author Ben Yu  * @author Gregory Kick  * @since 10.0 (<a href="https://github.com/google/guava/wiki/Compatibility">mostly  *     source-compatible</a> since 4.0)  */
 end_comment
 
 begin_class
