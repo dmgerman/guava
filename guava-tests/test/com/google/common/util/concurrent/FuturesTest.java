82884f9195a773664de727d6df348626c87df061
ashersb <ashersb@google.com>
2016-04-20 08:05:56 -0700

    Support reflection-less atomic updates in AggregateFutureState and InterruptibleTask as in AbstractFuture.
    Fixes https://github.com/google/guava/issues/2463
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=120337540
    
    
    Former-commit-id: 80dad6bf149f9b8d66e4db4436034cf6b7018398

--- guava-tests/test/com/google/common/util/concurrent/FuturesTest.java
+++ guava-tests/test/com/google/common/util/concurrent/FuturesTest.java
@@ -1104,7 +1104,7 @@ decl_stmt|;
 annotation|@
 name|Override
 DECL|method|setUp ()
-specifier|protected
+specifier|public
 name|void
 name|setUp
 parameter_list|()
@@ -1127,7 +1127,7 @@ block|}
 annotation|@
 name|Override
 DECL|method|tearDown ()
-specifier|protected
+specifier|public
 name|void
 name|tearDown
 parameter_list|()
