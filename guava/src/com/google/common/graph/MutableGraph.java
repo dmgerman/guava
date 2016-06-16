e28155d8898e6fef57b0e8ddd7caaea387a84b42
jasexton <jasexton@google.com>
2016-06-16 13:58:23 -0700

    Delete the existing Hypergraph interface. We're pretty much 100% sure this is not where we want it in the interface hierarchy, and leaving it there as a public interface isn't helping anyone.
    Also, remove references to "hyperedges" and "hypergraphs" in the javadoc/g3doc. No proposal that we're considering will Graph/Network support hyperedges.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=125098921
    
    
    Former-commit-id: f159e3fc4d9dec1beea0cc39cd887250e6ea36b5

--- guava/src/com/google/common/graph/MutableGraph.java
+++ guava/src/com/google/common/graph/MutableGraph.java
@@ -89,7 +89,7 @@ name|N
 name|nodeB
 parameter_list|)
 function_decl|;
-comment|/**    * Removes {@code node} from this graph, if it is present (optional operation).    * In general, all edges incident to {@code node} in this graph will also be removed.    * (This is not true for hyperedges.)    *    * @return {@code true} iff the graph was modified as a result of this call    * @throws UnsupportedOperationException if the remove operation is not supported by this graph    */
+comment|/**    * Removes {@code node} from this graph, if it is present (optional operation).    * All edges incident to {@code node} in this graph will also be removed.    *    * @return {@code true} iff the graph was modified as a result of this call    * @throws UnsupportedOperationException if the remove operation is not supported by this graph    */
 annotation|@
 name|CanIgnoreReturnValue
 DECL|method|removeNode (Object node)
