a29a1a249704f26785ebd826fa4da05bcb967726
cgdecker <cgdecker@google.com>
2016-12-08 20:41:48 -0800

    Add a disclaimer to Files/MoreFiles.getFileExtension that clarifies that it is unable to account for filesystem-specific quirks and instead only uses the information available in the File/Path API.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=141517772
    
    
    Former-commit-id: 5a42baf73ca755366e9aeff5567b2ef70d891852

--- guava/src/com/google/common/io/Files.java
+++ guava/src/com/google/common/io/Files.java
@@ -2524,7 +2524,7 @@ return|return
 name|result
 return|;
 block|}
-comment|/**    * Returns the<a href="http://en.wikipedia.org/wiki/Filename_extension">file extension</a> for    * the given file name, or the empty string if the file has no extension. The result does not    * include the '{@code .}'.    *    * @since 11.0    */
+comment|/**    * Returns the<a href="http://en.wikipedia.org/wiki/Filename_extension">file extension</a> for    * the given file name, or the empty string if the file has no extension. The result does not    * include the '{@code .}'.    *    *<p><b>Note:</b> This method simply returns everything after the last '{@code .}' in the file's    * name as determined by {@link File#getName}. It does not account for any filesystem-specific    * behavior that the {@link File} API does not already account for. For example, on NTFS it will    * report {@code "txt"} as the extension for the filename {@code "foo.exe:.txt"} even though NTFS    * will drop the {@code ":.txt"} part of the name when the file is actually created on the    * filesystem due to NTFS's<a href="https://goo.gl/vTpJi4">Alternate Data Streams</a>.    *    * @since 11.0    */
 DECL|method|getFileExtension (String fullName)
 specifier|public
 specifier|static
