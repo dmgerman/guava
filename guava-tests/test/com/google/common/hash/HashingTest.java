bed47a30ca0928b305c56aa84373d234c7552e7d
sunxin <sunxin@google.com>
2016-12-27 20:40:36 -0800

    Add a TODO; inadvertently update some copyright dates.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=143076499
    
    
    Former-commit-id: e7936c2c38761c81f78323244e5405e826dc50e6

--- guava-tests/test/com/google/common/hash/HashingTest.java
+++ guava-tests/test/com/google/common/hash/HashingTest.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (C) 2011 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Copyright (C) 2016 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -245,7 +245,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Unit tests for {@link Hashing}.  *  * @author Dimitris Andreou  * @author Kurt Alfred Kluever  */
+comment|/**  * Unit tests for {@link Hashing}.  *  *<p>TODO(b/33919189): Migrate repeated testing methods to {@link #HashTestUtils} and tweak unit  * tests to reference them from there.  *  * @author Dimitris Andreou  * @author Kurt Alfred Kluever  */
 end_comment
 
 begin_class
