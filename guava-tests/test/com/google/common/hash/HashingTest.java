ac4073d2cc803d81647b4ea0a24b306c9e435cb1
sunxin <sunxin@google.com>
2017-01-02 18:10:37 -0800

    Tweaks from shuffling of internal hash functions.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=143402767
    
    
    Former-commit-id: d9f088a61c80fa47d09ccf05a8c3608a47c5e4c0

--- guava-tests/test/com/google/common/hash/HashingTest.java
+++ guava-tests/test/com/google/common/hash/HashingTest.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (C) 2016 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Copyright (C) 2011 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -3594,6 +3594,22 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+comment|// The following legacy hashing function methods have been covered by unit testing already.
+name|List
+argument_list|<
+name|String
+argument_list|>
+name|legacyHashingMethodNames
+init|=
+name|ImmutableList
+operator|.
+name|of
+argument_list|(
+literal|"murmur2_64"
+argument_list|,
+literal|"fprint96"
+argument_list|)
+decl_stmt|;
 for|for
 control|(
 name|Method
@@ -3641,9 +3657,20 @@ operator|.
 name|length
 operator|==
 literal|0
+comment|// only the seed-less grapes^W hash functions
+operator|&&
+operator|!
+name|legacyHashingMethodNames
+operator|.
+name|contains
+argument_list|(
+name|method
+operator|.
+name|getName
+argument_list|()
+argument_list|)
 condition|)
 block|{
-comment|// only the seed-less grapes^W hash functions
 name|HashFunction
 name|hashFunction
 init|=
@@ -4472,6 +4499,7 @@ expr_stmt|;
 block|}
 block|}
 block|}
+comment|// Parity tests taken from //util/hash/hash_unittest.cc
 block|}
 end_class
 
