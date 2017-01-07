c4bba0063674d9030acffc28082b9b0a565ac5ca
jnyman <jnyman@google.com>
2017-01-07 07:29:17 -0800

    Deprecate [Binary]TreeTraverser.
    RELNOTES=Deprecated TreeTraverser and BinaryTreeTraverser. Please use com.google.common.graph.Traverser instead.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=173254759
    
    
    Former-commit-id: b72fe8611c3ed4b2782ca79dec170efe5d1fbd0e

--- android/guava/src/com/google/common/collect/BinaryTreeTraverser.java
+++ android/guava/src/com/google/common/collect/BinaryTreeTraverser.java
@@ -115,10 +115,12 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * A variant of {@link TreeTraverser} for binary trees, providing additional traversals specific to  * binary trees.  *  * @author Louis Wasserman  * @since 15.0  */
+comment|/**  * A variant of {@link TreeTraverser} for binary trees, providing additional traversals specific to  * binary trees.  *  * @author Louis Wasserman  * @since 15.0  * @deprecated Use {@link com.google.common.graph.Traverser} instead. All instance methods except  *     for {@link #inOrderTraversal} have their equivalent on the result of {@code  *     Traverser.forTree(tree)} where {@code tree} implements {@code SuccessorsFunction}, which has  *     a similar API as {@link #children}.  *<p>This class is scheduled to be removed in January 2018.  */
 end_comment
 
 begin_class
+annotation|@
+name|Deprecated
 annotation|@
 name|Beta
 annotation|@
