606f9b34993a87d83a07af50bce39d78df5e5381
sunxin <sunxin@google.com>
2017-01-23 14:14:27 -0800

    Among the Futures methods that run user callbacks, those that don't take an Executor will be deleted.  This CL migrates them to the counterparts that take MoreExecutors.directExecutor() as such Executor in the parameter list, exactly the way that the old method works.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=145333015
    
    
    Former-commit-id: 39a92f259efe0dc73221af42b8f7e5f84b0130f4

--- guava/src/com/google/common/cache/LocalCache.java
+++ guava/src/com/google/common/cache/LocalCache.java
@@ -92,6 +92,24 @@ name|util
 operator|.
 name|concurrent
 operator|.
+name|Futures
+operator|.
+name|transform
+import|;
+end_import
+
+begin_import
+import|import static
+name|com
+operator|.
+name|google
+operator|.
+name|common
+operator|.
+name|util
+operator|.
+name|concurrent
+operator|.
 name|MoreExecutors
 operator|.
 name|directExecutor
@@ -17032,8 +17050,6 @@ block|}
 comment|// To avoid a race, make sure the refreshed value is set into loadingValueReference
 comment|// *before* returning newValue from the cache query.
 return|return
-name|Futures
-operator|.
 name|transform
 argument_list|(
 name|newValue
@@ -17079,6 +17095,9 @@ name|newValue
 return|;
 block|}
 block|}
+argument_list|,
+name|directExecutor
+argument_list|()
 argument_list|)
 return|;
 block|}
