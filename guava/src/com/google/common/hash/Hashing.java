37810d2b003a2a34a7ac90847aebc95505a3e7c7
nickfelt <nickfelt@google.com>
2017-02-15 12:56:21 -0800

    Fix "SecretSpecKey" typo in Hashing javadoc
    The actual JDK class is named SecretKeySpec, not SecretSpecKey.  Arguably this is confusing since it does implement Key, but then again it also implements KeySpec...
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=147632964
    
    
    Former-commit-id: 6f96135d896e671b14f69c90d6d989f0652da1e6

--- guava/src/com/google/common/hash/Hashing.java
+++ guava/src/com/google/common/hash/Hashing.java
@@ -741,7 +741,7 @@ argument_list|)
 argument_list|)
 return|;
 block|}
-comment|/**    * Returns a hash function implementing the Message Authentication Code (MAC) algorithm, using the    * MD5 (128 hash bits) hash function and a {@link SecretSpecKey} created from the given byte array    * and the MD5 algorithm.    *    *    * @param key the key material of the secret key    * @since 20.0    */
+comment|/**    * Returns a hash function implementing the Message Authentication Code (MAC) algorithm, using the    * MD5 (128 hash bits) hash function and a {@link SecretKeySpec} created from the given byte array    * and the MD5 algorithm.    *    *    * @param key the key material of the secret key    * @since 20.0    */
 DECL|method|hmacMd5 (byte[] key)
 specifier|public
 specifier|static
@@ -797,7 +797,7 @@ argument_list|)
 argument_list|)
 return|;
 block|}
-comment|/**    * Returns a hash function implementing the Message Authentication Code (MAC) algorithm, using the    * SHA-1 (160 hash bits) hash function and a {@link SecretSpecKey} created from the given byte    * array and the SHA-1 algorithm.    *    *    * @param key the key material of the secret key    * @since 20.0    */
+comment|/**    * Returns a hash function implementing the Message Authentication Code (MAC) algorithm, using the    * SHA-1 (160 hash bits) hash function and a {@link SecretKeySpec} created from the given byte    * array and the SHA-1 algorithm.    *    *    * @param key the key material of the secret key    * @since 20.0    */
 DECL|method|hmacSha1 (byte[] key)
 specifier|public
 specifier|static
@@ -853,7 +853,7 @@ argument_list|)
 argument_list|)
 return|;
 block|}
-comment|/**    * Returns a hash function implementing the Message Authentication Code (MAC) algorithm, using the    * SHA-256 (256 hash bits) hash function and a {@link SecretSpecKey} created from the given byte    * array and the SHA-256 algorithm.    *    *    * @param key the key material of the secret key    * @since 20.0    */
+comment|/**    * Returns a hash function implementing the Message Authentication Code (MAC) algorithm, using the    * SHA-256 (256 hash bits) hash function and a {@link SecretKeySpec} created from the given byte    * array and the SHA-256 algorithm.    *    *    * @param key the key material of the secret key    * @since 20.0    */
 DECL|method|hmacSha256 (byte[] key)
 specifier|public
 specifier|static
@@ -909,7 +909,7 @@ argument_list|)
 argument_list|)
 return|;
 block|}
-comment|/**    * Returns a hash function implementing the Message Authentication Code (MAC) algorithm, using the    * SHA-512 (512 hash bits) hash function and a {@link SecretSpecKey} created from the given byte    * array and the SHA-512 algorithm.    *    *    * @param key the key material of the secret key    * @since 20.0    */
+comment|/**    * Returns a hash function implementing the Message Authentication Code (MAC) algorithm, using the    * SHA-512 (512 hash bits) hash function and a {@link SecretKeySpec} created from the given byte    * array and the SHA-512 algorithm.    *    *    * @param key the key material of the secret key    * @since 20.0    */
 DECL|method|hmacSha512 (byte[] key)
 specifier|public
 specifier|static
