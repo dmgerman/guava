aa7758718f8aa3ae5cd9f43c2220e779018152b8
yorick <yorick@google.com>
2017-09-28 06:34:41 -0700

    [guava concurrent] Updated a comment in ListenerCallQueue to point at SequentialExecutor instead of the deprecated SerializingExecutor wrapper interface.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=170335851
    
    
    Former-commit-id: 6d8d9d97f73cb5663f794edd7aaa1c7ede725203

--- android/guava/src/com/google/common/util/concurrent/ListenerCallQueue.java
+++ android/guava/src/com/google/common/util/concurrent/ListenerCallQueue.java
@@ -428,7 +428,7 @@ argument_list|()
 expr_stmt|;
 block|}
 block|}
-comment|/**    * A special purpose queue/executor that dispatches listener events serially on a configured    * executor. Each event event can be added and dispatched as separate phases.    *    *<p>This class is very similar to {@link SerializingExecutor} with the exception that events can    * be added without necessarily executing immediately.    */
+comment|/**    * A special purpose queue/executor that dispatches listener events serially on a configured    * executor. Each event event can be added and dispatched as separate phases.    *    *<p>This class is very similar to {@link SequentialExecutor} with the exception that events can    * be added without necessarily executing immediately.    */
 DECL|class|PerListenerQueue
 specifier|private
 specifier|static
