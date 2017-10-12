c92f5c9192fab084c03b3d28eb3d85302266e1b9
jianglai <jianglai@google.com>
2017-10-12 12:10:14 -0700

    Update hasPublicSuffix javadoc
    "google" is a public suffix, and "google.foo" is under public suffix "foo". Therefore hasPublicSuffix() should have returned true for both of them. In fact both TLDs (.google and .foo) are owned by Google Registry, i. e. us!
    
    I tested calling InternetDomainName.from("google").hasPublicSuffix(), which returns true as expected. Same goes for "google.foo".
    
    I suspect this javadoc has not been updated after the last round of gTLD expansion. This method really should only return false for domains that are under nonexistent TLDs, like "google.invalid", or fake TLDs themselves, like "invalid" (see https://en.wikipedia.org/wiki/.invalid).
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=171993370
    
    
    Former-commit-id: 0ed66d89488c7cdee558aa82b4422993e7bd2fa9

--- guava/src/com/google/common/net/InternetDomainName.java
+++ guava/src/com/google/common/net/InternetDomainName.java
@@ -856,7 +856,7 @@ operator|==
 literal|0
 return|;
 block|}
-comment|/**    * Indicates whether this domain name ends in a {@linkplain #isPublicSuffix() public suffix},    * including if it is a public suffix itself. For example, returns {@code true} for    * {@code www.google.com}, {@code foo.co.uk} and {@code com}, but not for {@code google} or    * {@code google.foo}. This is the recommended method for determining whether a domain is    * potentially an addressable host.    *    * @since 6.0    */
+comment|/**    * Indicates whether this domain name ends in a {@linkplain #isPublicSuffix() public suffix},    * including if it is a public suffix itself. For example, returns {@code true} for    * {@code www.google.com}, {@code foo.co.uk} and {@code com}, but not for {@code invalid} or    * {@code google.invalid}. This is the recommended method for determining whether a domain is    * potentially an addressable host.    *    * @since 6.0    */
 DECL|method|hasPublicSuffix ()
 specifier|public
 name|boolean
