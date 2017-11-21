a57c718abf95f1a059588d2e22e7250ba26e0687
jnyman <jnyman@google.com>
2017-11-21 08:48:07 -0800

    Carry @CanIgnoreReturnValue over to new methods.
    (Same as [] except for SimpleTimeLimiter)
    
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=176514048
    
    
    Former-commit-id: 17de0768eeefbbd8790407544a881e1cca9da5c3

--- android/guava/src/com/google/common/util/concurrent/SimpleTimeLimiter.java
+++ android/guava/src/com/google/common/util/concurrent/SimpleTimeLimiter.java
@@ -108,6 +108,20 @@ end_import
 
 begin_import
 import|import
+name|com
+operator|.
+name|google
+operator|.
+name|errorprone
+operator|.
+name|annotations
+operator|.
+name|CanIgnoreReturnValue
+import|;
+end_import
+
+begin_import
+import|import
 name|java
 operator|.
 name|lang
@@ -637,6 +651,8 @@ throw|;
 block|}
 block|}
 annotation|@
+name|CanIgnoreReturnValue
+annotation|@
 name|Override
 DECL|method|callWithTimeout (Callable<T> callable, long timeoutDuration, TimeUnit timeoutUnit)
 specifier|public
@@ -747,6 +763,8 @@ argument_list|()
 throw|;
 block|}
 block|}
+annotation|@
+name|CanIgnoreReturnValue
 annotation|@
 name|Override
 DECL|method|callUninterruptiblyWithTimeout ( Callable<T> callable, long timeoutDuration, TimeUnit timeoutUnit)
