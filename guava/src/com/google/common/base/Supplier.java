1aeea3e333d9d1c23b915a34dfe139ffee31b0f1
cpovirk <cpovirk@google.com>
2017-12-11 14:29:15 -0800

    Link to JDK Supplier, just as we link to JDK Function and Predicate.
    Change Function and Predicate to display the fully qualified name of the class they're linking to.
    
    Also link to the JDK versions of all those classes in the backport, which we're building with a Java 8 version of Javadoc and from which we're linking to Java 9 Javadoc.
    
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=178673695
    
    
    Former-commit-id: f58203fbc4ba0597fdbc7cfebe7ff2c5ce6ec1d4

--- guava/src/com/google/common/base/Supplier.java
+++ guava/src/com/google/common/base/Supplier.java
@@ -45,7 +45,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Legacy version of {@code java.util.function.Supplier}. Semantically, this could be a factory,  * generator, builder, closure, or something else entirely. No guarantees are implied by this  * interface.  *  *<p>The {@link Suppliers} class provides common suppliers and related utilities.  *  *<p>As this interface extends {@link java.util.function.Supplier}, an instance of this type can be  * used as a {@code java.util.function.Supplier} directly. To use a {@code  * java.util.function.Supplier} in a context where a {@code com.google.common.base.Supplier} is  * needed, use {@code supplier::get}.  *  *<p>See the Guava User Guide article on<a  * href="https://github.com/google/guava/wiki/FunctionalExplained">the use of {@code Function}</a>.  *  * @author Harry Heymann  * @since 2.0  */
+comment|/**  * Legacy version of {@link java.util.function.Supplier java.util.function.Supplier}. Semantically,  * this could be a factory, generator, builder, closure, or something else entirely. No guarantees  * are implied by this interface.  *  *<p>The {@link Suppliers} class provides common suppliers and related utilities.  *  *<p>As this interface extends {@code java.util.function.Supplier}, an instance of this type can be  * used as a {@code java.util.function.Supplier} directly. To use a {@code  * java.util.function.Supplier} in a context where a {@code com.google.common.base.Supplier} is  * needed, use {@code supplier::get}.  *  *<p>See the Guava User Guide article on<a  * href="https://github.com/google/guava/wiki/FunctionalExplained">the use of {@code Function}</a>.  *  * @author Harry Heymann  * @since 2.0  */
 end_comment
 
 begin_interface
@@ -75,6 +75,8 @@ block|{
 comment|/**    * Retrieves an instance of the appropriate type. The returned object may or may not be a new    * instance, depending on the implementation.    *    * @return an instance of the appropriate type    */
 annotation|@
 name|CanIgnoreReturnValue
+annotation|@
+name|Override
 DECL|method|get ()
 name|T
 name|get
