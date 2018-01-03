d5260d5e411b7c10ae787866794fdbf7dbde318b
cnsun <cnsun@google.com>
2018-01-03 13:42:11 -0800

    Add nullness annotations on the field in AbstractMultiset.java
    RELNOTES=n/a.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=180712315
    
    
    Former-commit-id: 092114536b415f8aca3150387fc5c02ab258f22d

--- guava/src/com/google/common/collect/AbstractMultiset.java
+++ guava/src/com/google/common/collect/AbstractMultiset.java
@@ -140,6 +140,22 @@ name|nullness
 operator|.
 name|compatqual
 operator|.
+name|MonotonicNonNullDecl
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|checkerframework
+operator|.
+name|checker
+operator|.
+name|nullness
+operator|.
+name|compatqual
+operator|.
 name|NullableDecl
 import|;
 end_import
@@ -545,6 +561,8 @@ expr_stmt|;
 block|}
 comment|// Views
 DECL|field|elementSet
+annotation|@
+name|MonotonicNonNullDecl
 specifier|private
 specifier|transient
 name|Set
@@ -655,6 +673,8 @@ name|distinctElements
 parameter_list|()
 function_decl|;
 DECL|field|entrySet
+annotation|@
+name|MonotonicNonNullDecl
 specifier|private
 specifier|transient
 name|Set
