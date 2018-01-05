b22ef499fd58f219b39958955d686622cd6f21b4
cnsun <cnsun@google.com>
2018-01-05 09:35:41 -0800

    Add nullness annotations on the fields in AbstractScheduledService.java
    RELNOTES=n/a.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=180937879
    
    
    Former-commit-id: 08e19cbd533582acfcd6c274146707e89004819e

--- android/guava/src/com/google/common/util/concurrent/AbstractScheduledService.java
+++ android/guava/src/com/google/common/util/concurrent/AbstractScheduledService.java
@@ -300,6 +300,22 @@ name|nullness
 operator|.
 name|compatqual
 operator|.
+name|MonotonicNonNullDecl
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|checkerframework
+operator|.
+name|checker
+operator|.
+name|nullness
+operator|.
+name|compatqual
+operator|.
 name|NullableDecl
 import|;
 end_import
@@ -553,6 +569,8 @@ block|{
 comment|// A handle to the running task so that we can stop it when a shutdown has been requested.
 comment|// These two fields are volatile because their values will be accessed from multiple threads.
 DECL|field|runningTask
+annotation|@
+name|MonotonicNonNullDecl
 specifier|private
 specifier|volatile
 name|Future
@@ -562,6 +580,8 @@ argument_list|>
 name|runningTask
 decl_stmt|;
 DECL|field|executorService
+annotation|@
+name|MonotonicNonNullDecl
 specifier|private
 specifier|volatile
 name|ScheduledExecutorService
