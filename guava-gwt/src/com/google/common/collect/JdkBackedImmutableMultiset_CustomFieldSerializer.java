489d01adc1bd4ad7c5c1b2ac4ef18a612af3eafd
lowasser <lowasser@google.com>
2018-02-09 12:05:27 -0800

    Add hash flooding protection to ImmutableMultiset, falling back to a JDK HashMap backed implementation when probable flooding is detected.
    While we're at it, rearchitect ImmutableMultiset on GWT to use that implementation rather than the handrolled hash table in RegularImmutableMultiset.
    
    RELNOTES=Add hash flooding protection to ImmutableMultiset, and significantly improve its GWT impl while we're at it.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=185173745
    
    
    Former-commit-id: afe6ff88023693d9c825b8d619dab56276fa8885

--- guava-gwt/src/com/google/common/collect/JdkBackedImmutableMultiset_CustomFieldSerializer.java
+++ guava-gwt/src/com/google/common/collect/JdkBackedImmutableMultiset_CustomFieldSerializer.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (C) 2011 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Copyright (C) 2018 The Guava Authors  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -71,14 +71,14 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * This class implements the GWT serialization of {@link RegularImmutableMultiset}.  *  * @author Louis Wasserman  */
+comment|/**  * This class implements the GWT serialization of {@link JdkBackedImmutableMultiset}.  *  * @author Louis Wasserman  */
 end_comment
 
 begin_class
-DECL|class|RegularImmutableMultiset_CustomFieldSerializer
+DECL|class|JdkBackedImmutableMultiset_CustomFieldSerializer
 specifier|public
 class|class
-name|RegularImmutableMultiset_CustomFieldSerializer
+name|JdkBackedImmutableMultiset_CustomFieldSerializer
 block|{
 DECL|method|deserialize (SerializationStreamReader reader, ImmutableMultiset<?> instance)
 specifier|public
