e41d6d8d54aaaf066adde2303ed141e88f876936
ghm <ghm@google.com>
2019-06-24 02:32:04 -0700

    Add more @Nullables to variadic parameter lists.
    RELNOTES=Add more @Nullables to variadic parameter lists.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=254718073
    
    
    Former-commit-id: 41253a22cef1f3708665375eb3a1f09921a8354d

--- guava/src/com/google/common/base/Preconditions.java
+++ guava/src/com/google/common/base/Preconditions.java
@@ -171,7 +171,7 @@ throw|;
 block|}
 block|}
 comment|/**    * Ensures the truth of an expression involving one or more parameters to the calling method.    *    * @param expression a boolean expression    * @param errorMessageTemplate a template for the exception message should the check fail. The    *     message is formed by replacing each {@code %s} placeholder in the template with an    *     argument. These are matched by position - the first {@code %s} gets {@code    *     errorMessageArgs[0]}, etc. Unmatched arguments will be appended to the formatted message in    *     square braces. Unmatched placeholders will be left as-is.    * @param errorMessageArgs the arguments to be substituted into the message template. Arguments    *     are converted to strings using {@link String#valueOf(Object)}.    * @throws IllegalArgumentException if {@code expression} is false    */
-DECL|method|checkArgument ( boolean expression, @Nullable String errorMessageTemplate, Object @Nullable ... errorMessageArgs)
+DECL|method|checkArgument ( boolean expression, @Nullable String errorMessageTemplate, @Nullable Object @Nullable ... errorMessageArgs)
 specifier|public
 specifier|static
 name|void
@@ -185,6 +185,8 @@ name|Nullable
 name|String
 name|errorMessageTemplate
 parameter_list|,
+annotation|@
+name|Nullable
 name|Object
 annotation|@
 name|Nullable
@@ -2411,7 +2413,7 @@ end_comment
 begin_expr_stmt
 unit|@
 name|CanIgnoreReturnValue
-DECL|method|checkNotNull ( T reference, @Nullable String errorMessageTemplate, Object @Nullable ... errorMessageArgs)
+DECL|method|checkNotNull ( T reference, @Nullable String errorMessageTemplate, @Nullable Object @Nullable ... errorMessageArgs)
 specifier|public
 specifier|static
 operator|<
@@ -2431,6 +2433,8 @@ name|Nullable
 name|String
 name|errorMessageTemplate
 argument_list|,
+annotation|@
+name|Nullable
 name|Object
 expr|@
 name|Nullable
