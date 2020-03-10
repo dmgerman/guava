2ccc84cb3440d6581ffd76137c93b343cf471efc
jnyman <jnyman@google.com>
2020-03-10 04:47:17 -0700

    Move all tests from StandardMutableDirectedNetworkTest to AbstractStandardDirectedNetworkTest
    This is similar to an earlier refector of the AbstractGraphTest tree.
    
    Note: This is a pure refactor: No tests were added or removed.
    
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=300063227
    
    
    Former-commit-id: f0575b14482e43f91e4c55c5da424e74914173ca

--- android/guava-tests/test/com/google/common/graph/StandardDirectedMultiNetworkTest.java
+++ android/guava-tests/test/com/google/common/graph/StandardDirectedMultiNetworkTest.java
@@ -111,7 +111,7 @@ specifier|public
 class|class
 name|StandardDirectedMultiNetworkTest
 extends|extends
-name|StandardMutableDirectedNetworkTest
+name|AbstractStandardDirectedNetworkTest
 block|{
 annotation|@
 name|Override
