c50eb97fd4c1595fa27706c1a89258e6c80717c4
cpovirk <cpovirk@google.com>
2021-05-05 12:53:16 -0700

    Document when `EndpointPair.adjacentNode` changed signature.
    (Followup to CL 371380054.)
    
    As usual, the scheme is:
    
    ```
    @since version-binary-compatible-since (other-clarifications)
    ```
    
    And in fact this change is binary-compatible, since the erasure of `N` is `Object`, the old parameter type. And while this change was potentially source-incompatible, we knew that and tested for problems internally, and we saw no breakages.
    
    Compare to [the `@since` tag for `Queues.newPriorityBlockingQueue`](https://github.com/google/guava/blob/dd3405bc4d191338aa008cd136da17ccf0ae96e1 [formerly 1799bc3e48db5ea61ba66191096727d17d1e811c]/guava/src/com/google/common/collect/Queues.java#L191).
    
    RELNOTES=n/a
    PiperOrigin-RevId: 372188501
    
    Former-commit-id: 5fec0fff378689878abd91cf3e68b54927bed637

--- android/guava/src/com/google/common/graph/EndpointPair.java
+++ android/guava/src/com/google/common/graph/EndpointPair.java
@@ -403,7 +403,7 @@ return|return
 name|nodeV
 return|;
 block|}
-comment|/**    * Returns the node that is adjacent to {@code node} along the origin edge.    *    * @throws IllegalArgumentException if this {@link EndpointPair} does not contain {@code node}    */
+comment|/**    * Returns the node that is adjacent to {@code node} along the origin edge.    *    * @throws IllegalArgumentException if this {@link EndpointPair} does not contain {@code node}    * @since 20.0 (requires that the argument have type {@code N} since 31.0)    */
 DECL|method|adjacentNode (N node)
 specifier|public
 specifier|final
